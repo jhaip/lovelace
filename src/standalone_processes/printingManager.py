c758d3e6523d663c9a56daa39e5267f01994239c
Jacob Haip <haipjacob@gmail.com>
2018-07-25 23:12:55 -0400

    first printingManager test

--- src/standalone_processes/printingManager.py
+++ src/standalone_processes/printingManager.py
@@ -20,9 +20,9 @@ def select(fact):
 while True:
     print_wishes = select('wish $name would be printed')
     for wish in print_wishes:
-        print("should print:")
-        print(wish)
-        # name = wish['name']['word']
-        # retract('wish {} would be printed'.format(name))
-        # subprocess.call(['/usr/bin/lpr', 'src/standalone_processes/{}'.format(name)])
+        name = wish['name']['word']
+        if '.py' not in name and '.js' not in name:
+            name += '.js'
+        retract('wish {} would be printed'.format(name))
+        subprocess.call(['/usr/bin/lpr', 'src/standalone_processes/{}'.format(name)])
     time.sleep(1)
