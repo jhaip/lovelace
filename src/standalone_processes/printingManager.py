b0152250b1572c51ef4c8577006e07994338afe5
Jacob Haip <haipjacob@gmail.com>
2018-07-26 03:44:13 +0000

    log to file in Python with logging module

--- src/standalone_processes/printingManager.py
+++ src/standalone_processes/printingManager.py
@@ -3,18 +3,17 @@ import requests
 import time
 import sys
 import os
+import logging
 
 scriptName = os.path.basename(__file__)
 scriptNameNoExtension = os.path.splitext(scriptName)[0]
-fileDir = os.path.dirname(os.path.realpath('__file__'))
+fileDir = os.path.dirname(os.path.realpath(__file__))
 logPath = os.path.join(fileDir, 'logs/' + scriptNameNoExtension + '.log')
 print(logPath)
 
-log_file = open(logPath, 'w')
-sys.stdout = log_file
-sys.stderr = log_file
+logging.basicConfig(filename=logPath, level=logging.INFO)
 
-print("printingManager started!")
+logging.info("printingManager started!")
 
 URL = "http://localhost:3000/"
 
@@ -32,13 +31,13 @@ def select(fact):
     return response.json()
 
 while True:
-    print("checking for printing wishes")
+    logging.info("checking for printing wishes")
     print_wishes = select('wish $name would be printed')
     for wish in print_wishes:
         name = wish['name']['word']
         retract('wish {} would be printed'.format(name))
         if '.py' not in name and '.js' not in name:
             name += '.js'
-        print("PRINTING:", name)
+        logging.info("PRINTING:", name)
         subprocess.call(['/usr/bin/lpr', 'src/standalone_processes/{}'.format(name)])
     time.sleep(1)
