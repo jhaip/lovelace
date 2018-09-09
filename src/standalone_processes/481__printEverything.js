ebdde7aa2d06d4dc1f0fecd9b2da58ded62f5039
Jacob Haip <haipjacob@gmail.com>
2018-09-08 21:49:03 -0400

    print everything wish

---
+++ src/standalone_processes/481__printEverything.js
@@ -0,0 +1,26 @@
+const Room = require('@living-room/client-js')
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const scriptName = path.basename(__filename);
+const scriptNameNoExtension = path.parse(scriptName).name;
+const logPath = __filename.replace(scriptName, 'logs/' + scriptNameNoExtension + '.log')
+const access = fs.createWriteStream(logPath)
+process.stdout.write = process.stderr.write = access.write.bind(access);
+process.on('uncaughtException', function(err) {
+  console.error((err && err.stack) ? err.stack : err);
+})
+const myId = (scriptName.split(".")[0]).split("__")[0]
+
+const room = new Room()
+
+room.on(`$ wish everything would be printed`, async (options) => {
+  const papers = (await room.select(`$ $processName has paper ID $paperId`))
+  console.log("papers:")
+  console.log(papers);
+  papers.forEach(p => {
+    const processName = p.processName.word || p.processName.value;
+    room.assert(`#${myId} wish paper ${p.paperId.value} at "${processName}" would be printed`)
+  })
+});
