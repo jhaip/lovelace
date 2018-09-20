9d47e549c08dc1e4f3b7d2fa7a83d3dc00f80e17
Jacob Haip <jhaip@users.noreply.github.com>
2018-09-20 18:03:36 -0400

    fix nested string parsing

--- new-backend/parser.py
+++ new-backend/parser.py
@@ -2,12 +2,16 @@ import os
 from arpeggio import *
 from arpeggio import RegExMatch as _
 
+# Escaped strings inspired from this:
+# https://github.com/PhilippeSigaud/Pegged/blob/master/pegged/examples/strings.d
+
 def whitespace():       return _('\s')
 def postfix():          return '$+'
 def wildcard():         return '$'
 def variable():         return wildcard, _('\w+')
 def postfixvariable():  return postfix, _('\w+')
-def string():           return '"', _('((\\")|([^"]))*'), '"'
+def char():             return [('\\', ['"', '\'', '\\']), _('.')]
+def string():           return '"', ZeroOrMore(Not('"'), char), '"'
 def value():            return  _('[^"\s]+')
 def number():           return _('-?\d+((\.\d*)?((e|E)(\+|-)?\d+)?)?')
 def id():               return '#', _('\d+')
@@ -20,7 +24,7 @@ def main(debug=False):
     testdata = '#0 bird1 "has" 8 toes'
     testdata1 = '#0 bird1 "has" 8 nice toes 0.5 1. .99999 1.23e8'
     testdata2 = '$ $Animal "has" $+TheRest1'
-    testdata3 = '#0 "This \\"is\\" a test"'
+    testdata3 = '#0 "This \\"is\\" a test" one "two"'
     print(testdata3)
     parse_tree = parser.parse(testdata3)
     # parse_tree can now be analysed and transformed to some other form
