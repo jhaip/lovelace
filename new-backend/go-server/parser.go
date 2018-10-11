ef117bf9c46447431edec75688ee51a077e3875a
Jacob Haip <haipjacob@gmail.com>
2018-10-11 19:29:47 -0400

    separate parser into new file, add parser test

---
+++ new-backend/go-server/parser.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/alecthomas/participle"
+)
+
+// type Term struct {
+// 	Type  string
+// 	Value string
+// }
+
+// Grammar Start
+type ParsedFact struct {
+	FactTerms []*FactTerm `{ @@ }`
+}
+
+type Postfix struct {
+	Postfix string `"%"[@Ident]`
+}
+
+type Wildcard struct {
+	Wildcard string `"$"[@Ident]`
+}
+
+type Number struct {
+	Number float64 `@Float`
+}
+
+type Integer struct {
+	Integer int `@Int`
+}
+
+type FactTerm struct {
+	Postfix  *Postfix  `@@ |`
+	Wildcard *Wildcard `@@ |`
+	Id       string    `"#"(@Ident|@Int) |`
+	String   string    `@String |`
+	Integer  *Integer  `@@ |`
+	Number   *Number   `@@ |`
+	Value    string    `@Ident`
+}
+
+//
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func make_parser() (*participle.Parser, error) {
+	return participle.Build(&ParsedFact{})
+}
+
+func parse_fact_string(parser *participle.Parser, fact_string string) []Term {
+	// fmt.Println("PARSE", fact_string)
+	fact := &ParsedFact{}
+	err := parser.ParseString(fact_string, fact)
+	checkErr(err)
+	fact_terms := make([]Term, 0)
+	for _, fact_term := range (*fact).FactTerms {
+		if fact_term.String != "" {
+			fact_terms = append(fact_terms, Term{"text", fact_term.String})
+		}
+		if fact_term.Value != "" {
+			fact_terms = append(fact_terms, Term{"text", fact_term.Value})
+		}
+		if fact_term.Id != "" {
+			fact_terms = append(fact_terms, Term{"id", fact_term.Id})
+		}
+		if fact_term.Integer != nil {
+			fact_terms = append(fact_terms, Term{"integer", fmt.Sprintf("%v", (*fact_term.Integer).Integer)})
+		}
+		if fact_term.Number != nil {
+			fact_terms = append(fact_terms, Term{"float", fmt.Sprintf("%f", (*fact_term.Number).Number)})
+		}
+		if fact_term.Wildcard != nil {
+			if fact_term.Wildcard == nil {
+				fact_terms = append(fact_terms, Term{"variable", ""})
+			} else {
+				fact_terms = append(fact_terms, Term{"variable", (*fact_term.Wildcard).Wildcard})
+			}
+		}
+		if fact_term.Postfix != nil {
+			if fact_term.Postfix == nil {
+				fact_terms = append(fact_terms, Term{"postfix", ""})
+			} else {
+				fact_terms = append(fact_terms, Term{"postfix", (*fact_term.Postfix).Postfix})
+			}
+		}
+		// repr.Println(fact_term, repr.Indent("  "), repr.OmitEmpty(true))
+	}
+	// fmt.Println(fact_terms)
+	return fact_terms
+}
---
+++ new-backend/go-server/parser_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/alecthomas/repr"
+)
+
+func TestParse(t *testing.T) {
+	parser, _ := make_parser()
+	terms := parse_fact_string(parser, "#P5 #0 \"This \\\"is\\\" a test\" one \"two\" 0.5 2 1. .99999 1.23e8 $ $X % %Z")
+	repr.Println(terms, repr.Indent("  "), repr.OmitEmpty(true))
+	expected_terms := []Term{
+		Term{"id", "P5"},
+		Term{"id", "0"},
+		Term{"text", "This \"is\" a test"},
+		Term{"text", "one"},
+		Term{"text", "two"},
+		Term{"float", "0.500000"},
+		Term{"integer", "2"},
+		Term{"float", "1.000000"},
+		Term{"float", "0.999990"},
+		Term{"float", "123000000.000000"},
+		Term{"variable", ""},
+		Term{"variable", "X"},
+		Term{"postfix", ""},
+		Term{"postfix", "Z"},
+	}
+	if len(terms) != len(expected_terms) {
+		t.Error("Wrong number of terms")
+	}
+	for i, term := range terms {
+		if term.Type != expected_terms[i].Type {
+			t.Error("Wrong term type for term ", i)
+		}
+		if term.Value != expected_terms[i].Value {
+			t.Error("Wrong term value for term ", i)
+		}
+	}
+}
