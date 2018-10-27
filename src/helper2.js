06b5b7f919b43b5e25c2156db53b124f1eb37f37
Jacob Haip <haipjacob@gmail.com>
2018-10-26 23:54:55 -0400

    re-enable logging to file in helper2.js

--- src/helper2.js
+++ src/helper2.js
@@ -54,10 +54,10 @@ function init(filename) {
     const scriptNameNoExtension = path.parse(scriptName).name;
     const logPath = filename.replace(scriptName, 'logs/' + scriptNameNoExtension + '.log')
     const access = fs.createWriteStream(logPath)
-    // process.stdout.write = process.stderr.write = access.write.bind(access);
-    // process.on('uncaughtException', function (err) {
-    //     console.error((err && err.stack) ? err.stack : err);
-    // })
+    process.stdout.write = process.stderr.write = access.write.bind(access);
+    process.on('uncaughtException', function (err) {
+        console.error((err && err.stack) ? err.stack : err);
+    })
     const myId = (scriptName.split(".")[0]).split("__")[0]
 
     const subscriber = zmq.socket('sub');
