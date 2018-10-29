5b911ecd900f893a7a40ce06446e0437296e1640
Jacob Haip <haipjacob@gmail.com>
2018-10-29 11:35:30 -0400

    use integers instead of floats in 2__test.js

--- src/standalone_processes/2__test.js
+++ src/standalone_processes/2__test.js
@@ -6,6 +6,7 @@ let i = 1
 room.on(
     `$ $X ${myId} has $Y toes`,
     results => {
+        room.cleanup()
         console.log("results:")
         console.log(results)
         i += 1;
@@ -13,13 +14,13 @@ room.on(
             console.log("\n...DONE!")
             process.exit(0);
         }
-        room.assert(`Man ${(myId * 1.0).toFixed(6)} has ${i} toes`)
-        room.assert(`Man ${(myId * 1.0).toFixed(6)} has ${i} toes`)
+        room.assert(`Man ${myId} has ${i} toes`)
+        room.assert(`Man ${myId} has ${i} toes`)
         room.assert(`Hello`, ["text", "world"], "$")
     }
 )
 
-room.assert(`Man ${(myId * 1.0).toFixed(6)} has 0 toes`)
+room.assert(`Man ${myId} has 0 toes`)
 room.assert(`Hello`, ["text", "world"], "$")
 
 run();
