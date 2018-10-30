6da30f97645b643cb332b778f1b77f32597c5c69
Jacob Haip <haipjacob@gmail.com>
2018-10-30 19:28:41 -0400

    add switch to enable and disable long polling the db

--- web-debugger/index.js
+++ web-debugger/index.js
@@ -1,3 +1,21 @@
+let longPollingActive = true;
+
+function updateLongPolling(newValue) {
+    if (newValue && !longPollingActive) {
+        poll();
+    }
+    longPollingActive = newValue;
+    if (longPollingActive) {
+        $(".long-poll-button").addClass("long-poll-button--active")
+    } else {
+        $(".long-poll-button").removeClass("long-poll-button--active")
+    }
+}
+
+$(".long-poll-button").click(function () {
+    updateLongPolling(!longPollingActive);
+})
+
 function b64DecodeUnicode(str) {
     // Going backwards: from bytestream, to percent-encoding, to original string.
     return decodeURIComponent(atob(str).split('').map(function (c) {
@@ -5,44 +23,57 @@ function b64DecodeUnicode(str) {
     }).join(''));
 }
 
-$.get("http://localhost:3000/db", function (data) {
-    // console.log(data)
-    const lastValSlice = data.slice(-1)
-    if (lastValSlice.length === 1 && lastValSlice[0] == "") {
-        data = data.slice(0, -1)
-    }
-    // console.log(data)
-    var decodedData = data.map(function (datum) {
-        // console.log(datum);
-        const parsedDatum = JSON.parse(datum).map(function (d) {
-            if (d[0] === "text") {
-                return [d[0], b64DecodeUnicode(d[1])]
-            }
-            return d;
-        });
-        return parsedDatum;
-    })
-    // console.log(decodedData)
-    // var decodedData = data.map(b64DecodeUnicode)
-    var dataJoinedBySource = {}
-    decodedData.forEach(function (data) {
-        var firstWord = data[0][1]; // value of the first datum
-        if (!(firstWord in dataJoinedBySource)) {
-            dataJoinedBySource[firstWord] = []    
+function poll() {
+    $.get("http://localhost:3000/db", function (data) {
+        if (!longPollingActive) {
+            return;
         }
-        dataJoinedBySource[firstWord].push(data)
-    })
-    // console.log(decodedData)
+        // console.log(data)
+        const lastValSlice = data.slice(-1)
+        if (lastValSlice.length === 1 && lastValSlice[0] == "") {
+            data = data.slice(0, -1)
+        }
+        // console.log(data)
+        var decodedData = data.map(function (datum) {
+            // console.log(datum);
+            const parsedDatum = JSON.parse(datum).map(function (d) {
+                if (d[0] === "text") {
+                    return [d[0], b64DecodeUnicode(d[1])]
+                }
+                return d;
+            });
+            return parsedDatum;
+        })
+        // console.log(decodedData)
+        // var decodedData = data.map(b64DecodeUnicode)
+        var dataJoinedBySource = {}
+        decodedData.forEach(function (data) {
+            var firstWord = data[0][1]; // value of the first datum
+            if (!(firstWord in dataJoinedBySource)) {
+                dataJoinedBySource[firstWord] = []    
+            }
+            dataJoinedBySource[firstWord].push(data)
+        })
+        // console.log(decodedData)
 
-    var decodedDataHTML = ""
-    Object.keys(dataJoinedBySource).forEach(function (source) {
-        decodedDataHTML += `<h4>${source}</h4>`
-        decodedDataHTML += dataJoinedBySource[source].map(function (data) {
-            const innerContents = data.map(function (d) {
-                return `<div class="val-type val-type--${d[0]}">${d[1]}</div>`
-            }).join("")
-            return `<li>${innerContents}</li>`
-        }).join('\n');
-    })
-    $(".results").html(decodedDataHTML);
-});
+        var decodedDataHTML = ""
+        Object.keys(dataJoinedBySource).forEach(function (source) {
+            decodedDataHTML += `<h4>${source}</h4>`
+            decodedDataHTML += dataJoinedBySource[source].map(function (data) {
+                const innerContents = data.map(function (d) {
+                    return `<div class="val-type val-type--${d[0]}">${d[1]}</div>`
+                }).join("")
+                return `<li>${innerContents}</li>`
+            }).join('\n');
+        })
+        $(".results").html(decodedDataHTML);
+
+        if (longPollingActive) {
+            setTimeout(function () {
+                poll();
+            }, 1000);
+        }
+    });
+}
+
+poll();
