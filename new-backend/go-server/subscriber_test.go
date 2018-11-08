5206e3af03de5f8c3d6dcc820469d8745b44d7a7
Jacob Haip <haipjacob@gmail.com>
2018-11-08 00:09:56 -0500

    complete all claim & retract tests - checking all expected nodes

--- new-backend/go-server/subscriber_test.go
+++ new-backend/go-server/subscriber_test.go
@@ -171,28 +171,48 @@ func TestMakeSubscriberOnePart(t *testing.T) {
 	if !reflect.DeepEqual(subscription.queryPartToUpdate[0], expected_queryPartToUpdate0) {
 		t.Error("subscription.queryPartToUpdate[0] does not match")
 	}
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
+	// repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
 
 	claim := []Term{Term{"text", "Sun"}, Term{"text", "is"}, Term{"text", "yellow"}}
 	subscription = subscriberClaimUpdate(subscription, claim)
 
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
+	expected_nodes["*query0 A B"].variableCache["*query0"] = []NodeValue{NodeValue{claim}}
+	expected_nodes["*query0 A B"].variableCache["A"] = []NodeValue{NodeValue{[]Term{Term{"text", "Sun"}}}}
+	expected_nodes["*query0 A B"].variableCache["B"] = []NodeValue{NodeValue{[]Term{Term{"text", "yellow"}}}}
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
+
+	// repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
 
 	// claim does not match
 	claim2 := []Term{Term{"text", "Sun"}, Term{"text", "BAD"}, Term{"text", "yellow"}}
 	subscription = subscriberClaimUpdate(subscription, claim2)
 
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
+
+	// repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
 
 	// $ is BAD does not match any previous claim, so this should do nothing
 	retract1 := []Term{Term{"variable", ""}, Term{"text", "is"}, Term{"text", "BAD"}}
 	subscription = subscriberRetractUpdate(subscription, retract1)
 
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
+
+	// repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
 
 	// $ is yellow matches a previous claim "Sun is yellow" so that claim should be removed
 	retract2 := []Term{Term{"variable", ""}, Term{"text", "is"}, Term{"text", "yellow"}}
 	subscription = subscriberRetractUpdate(subscription, retract2)
+
+	expected_nodes["*query0 A B"] = makeNodeFromVariableNames([]string{"*query0", "A", "B"})
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
 
 	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
 }
@@ -220,12 +240,22 @@ func TestMakeSubscriberOnePartNoVariables(t *testing.T) {
 	if !reflect.DeepEqual(subscription.queryPartToUpdate[0], expected_queryPartToUpdate0) {
 		t.Error("subscription.queryPartToUpdate[0] does not match")
 	}
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
 
 	claim := []Term{Term{"text", "Sun"}, Term{"text", "is"}, Term{"text", "yellow"}}
 	subscription = subscriberClaimUpdate(subscription, claim)
 
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
+	expected_nodes["*query0"].variableCache["*query0"] = []NodeValue{NodeValue{claim}}
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
+
+	retract1 := []Term{Term{"postfix", ""}}
+	subscription = subscriberRetractUpdate(subscription, retract1)
+
+	expected_nodes["*query0"] = makeNodeFromVariableNames([]string{"*query0"})
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
 }
 
 func TestMakeSubscriberTwoPartsNoVariables(t *testing.T) {
@@ -265,13 +295,42 @@ func TestMakeSubscriberTwoPartsNoVariables(t *testing.T) {
 	if !reflect.DeepEqual(subscription.queryPartToUpdate[1], expected_queryPartToUpdate1) {
 		t.Error("subscription.queryPartToUpdate[1] does not match")
 	}
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
+	// repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
 
 	claim := []Term{Term{"text", "Sun"}, Term{"text", "is"}, Term{"text", "yellow"}}
 	subscription = subscriberClaimUpdate(subscription, claim)
 
+	expected_nodes["*query0"].variableCache["*query0"] = []NodeValue{NodeValue{claim}}
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
+
 	claim2 := []Term{Term{"text", "Earth"}, Term{"text", "is"}, Term{"text", "green"}}
 	subscription = subscriberClaimUpdate(subscription, claim2)
 
-	repr.Println(subscription, repr.Indent("  "), repr.OmitEmpty(true))
+	expected_nodes["*query1"].variableCache["*query1"] = []NodeValue{NodeValue{claim2}}
+	expected_nodes["*query0 *query1"].variableCache["*query0"] = []NodeValue{NodeValue{claim}}
+	expected_nodes["*query0 *query1"].variableCache["*query1"] = []NodeValue{NodeValue{claim2}}
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
+
+	retract1 := []Term{Term{"variable", ""}, Term{"text", "is"}, Term{"text", "yellow"}}
+	subscription = subscriberRetractUpdate(subscription, retract1)
+
+	expected_nodes["*query0"] = makeNodeFromVariableNames([]string{"*query0"})
+	expected_nodes["*query0 *query1"] = makeNodeFromVariableNames([]string{"*query0", "*query1"})
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
+
+	retract2 := []Term{Term{"text", "Earth"}, Term{"text", "is"}, Term{"text", "green"}}
+	subscription = subscriberRetractUpdate(subscription, retract2)
+
+	expected_nodes["*query0"] = makeNodeFromVariableNames([]string{"*query0"})
+	expected_nodes["*query1"] = makeNodeFromVariableNames([]string{"*query1"})
+	expected_nodes["*query0 *query1"] = makeNodeFromVariableNames([]string{"*query0", "*query1"})
+	if !reflect.DeepEqual(subscription.nodes, expected_nodes) {
+		t.Error("Contents of nodes is wrong", subscription.nodes)
+	}
 }
