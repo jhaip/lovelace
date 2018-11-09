4634f13e0cabaaaf9cfc7be42b7f56952f977a41
Jacob Haip <haipjacob@gmail.com>
2018-11-09 00:48:16 -0500

    spawn not exec files in 1900__processManager to avoid node killing the process when its output buffer gets too large

--- src/standalone_processes/1900__processManager.js
+++ src/standalone_processes/1900__processManager.js
@@ -1,4 +1,4 @@
-const execFile = require('child_process').execFile;
+const spawn = require('child_process').spawn;
 const process = require('process');
 const path = require('path');
 const { room, myId, scriptName, run, MY_ID_STR } = require('../helper2')(__filename);
@@ -14,23 +14,24 @@ function runPaper(name) {
     languageProcess = 'go'
     runArgs = ['run', programSource]
   }
-  const child = execFile(
-    languageProcess,
-    runArgs,
-    (error, stdout, stderr) => {
-      // TODO: check if program should still be running
-      // and start it again if so.
-      console.error("program died:")
-      console.error(name);
-      console.error([["id", MY_ID_STR], ["text", name], `has process id $`])
-      room.retract(["id", MY_ID_STR], ["text", name], `has process id $`);
-      console.log(`${name} callback`)
-      if (error) {
-        console.error('stderr', stderr);
-        console.error(error);
-      }
-      console.log('stdout', stdout);
-    });
+  const child = spawn(languageProcess, runArgs)
+  // child.stdout.on('data', (data) => {
+  //   console.error(`stdout for ${name}`);
+  //   console.log(`stdout: ${data}`);
+  // });
+  // child.stderr.on('data', (data) => {
+  //   console.error(`stderr for ${name}`);
+  //   console.log(`stderr: ${data}`);
+  // });
+  child.on('close', (code) => {
+    // TODO: check if program should still be running
+    // and start it again if so.
+    console.error("program died:")
+    console.error(name);
+    console.error([["id", MY_ID_STR], ["text", name], `has process id $`])
+    room.retract(["id", MY_ID_STR], ["text", name], `has process id $`);
+    room.flush();
+  });
   const pid = child.pid;
   room.assert(["text", name], `has process id ${pid}`);
   console.error(pid);
@@ -78,9 +79,26 @@ room.on(
         stopPaper(name, nameToProcessIdCache[name])
       }
     }
+    room.assert(`processManager update ${(new Date()).toISOString()}`)
   }
 )
 
 room.assert(["text", path.basename(__filename)], `has process id ${process.pid}`);
 
 run()
+
+// TODO:
+// this file is getting a 
+/*
+1800__dots-to-papers.go callback
+stderr
+{ RangeError [ERR_CHILD_PROCESS_STDIO_MAXBUFFER]: stdout maxBuffer length exceeded
+    at Socket.onChildStdout (child_process.js:345:14)
+    at Socket.emit (events.js:182:13)
+    at addChunk (_stream_readable.js:283:12)
+    at readableAddChunk (_stream_readable.js:260:13)
+    at Socket.Readable.push (_stream_readable.js:219:10)
+    at Pipe.onread (net.js:638:20)
+  cmd: 'go run src/standalone_processes/1800__dots-to-papers.go' }
+*/
+// error, possible when 1800__dots-to-papers dies
