4bf9d6e3e84a39d92853ccdc549ec0cfff5501e9
Jacob Haip <haipjacob@gmail.com>
2018-11-11 21:31:00 -0500

    trim #777 to find in 74 lines (one paper)

--- src/standalone_processes/777__soundPlayer.py
+++ src/standalone_processes/777__soundPlayer.py
@@ -12,80 +12,50 @@ INSTRUMENT = 0
 @subscription(["$ melody is %melody"])
 def melody_callback(results):
     global MELODY, MELODY_INDEX
-    logging.error("results:")
-    logging.error(results)
-    if len(results) == 0:
-        MELODY = []
-        return
-    result = results[0]
-    if not result or result.get("melody", '') == '':
-        MELODY = []
-        return
     try:
-        result_melody = result["melody"].replace(" ", "").split(",")
-        result_melody = list(map(lambda x: int(x), result_melody))
+        result_melody = results[0]["melody"].replace(" ", "").split(",")
+        MELODY = list(map(lambda x: int(x), result_melody))
         MELODY_INDEX = 0
-        MELODY = result_melody
-        logging.error("new melody:")
-        logging.error(MELODY)
+        logging.error("new melody: {}".format(MELODY))
     except:
-        logging.error("bad melody {}".format(result["melody"]))
+        MELODY = []
+        logging.error("error finding melody in {}".format(results))
 
 @subscription(["$ beats per minute is $bpm"])
 def bpm_callback(results):
     global BPM
-    logging.error("results:")
-    logging.error(results)
-    if len(results) == 0:
-        BPM = 60
-        return
-    result = results[0]
     try:
-        result_bpm = int(result["bpm"])
-        BPM = result_bpm
-        logging.error("new bpm:")
-        logging.error(BPM)
+        BPM = int(results[0]["bpm"])
+        logging.error("new bpm: {}".format(BPM))
     except:
-        logging.error("bad BPM {}".format(result["bpm"]))
-
+        BPM = 60
+        logging.error("error finding BPM in {}".format(results))
 
 @subscription(["$ instrument is $instrument"])
 def instrument_callback(results):
     global INSTRUMENT
-    logging.error("results:")
-    logging.error(results)
-    if len(results) == 0:
-        INSTRUMENT = 0
-        return
     try:
-        result_instrument = int(results[0]["instrument"])
-        if result_instrument > 127 or result_instrument < 0:
-            result_instrument = 0
-        INSTRUMENT = result_instrument
-        logging.error("new instrument:")
-        logging.error(INSTRUMENT)
+        INSTRUMENT = int(results[0]["instrument"])
+        if INSTRUMENT > 127 or INSTRUMENT < 0:
+            INSTRUMENT = 0
+        logging.error("new instrument: {}".format(INSTRUMENT))
     except:
-        logging.error("BAD instrument {}".format(results))
+        INSTRUMENT = 0
+        logging.error("error finding instrument in {}".format(results))
 
-def playNextNode(midi_out, instrument, pitch, duration):
-    logging.info("playing stuff")
+def playNextNode(midi_out, instrument, pitch, duration_s):
     midi_out.set_instrument(instrument)
     # 74 is middle C, 127 is "how loud" - max is 127
     midi_out.note_on(pitch, 127)
-    time.sleep(duration)
+    time.sleep(duration_s)
     midi_out.note_off(pitch, 127)
-    # time.sleep(.5)
-
 
 init(__file__, skipListening=True)
-
 pygame.init()
 pygame.midi.init()
-
 port = pygame.midi.get_default_output_id()
 logging.info("using output_id :%s:" % port)
 midi_out = pygame.midi.Output(port, 0)
-
 try:
     lastUpdateTime = time.time()
     while True:
