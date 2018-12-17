a92141e75d8f5226f2d65561edd86b71989db41a
Jacob Haip <haipjacob@gmail.com>
2018-12-16 23:31:38 -0500

    save current state of #2000 - #2009 editable boot programs from today's testing

--- src/standalone_processes/2004__blank.js
+++ src/standalone_processes/2004__blank.js
@@ -1,3 +1,18 @@
 const { room, myId, run } = require('../helper2')(__filename);
 
 // Write code here!
+const DELAY_MS = 100;
+
+function tick() {
+  room.cleanup()
+  room.assert(`time is ${(new Date()).getTime()}`);
+  let ill = room.newIllumination()
+  ill.text(0, 100, "I am a clock")
+  room.draw(ill);
+  room.flush();
+  setTimeout(tick, DELAY_MS);
+}
+
+tick();
+
+run();
--- src/standalone_processes/2008__blank.js
+++ src/standalone_processes/2008__blank.js
@@ -1,3 +1,12 @@
 const { room, myId, run } = require('../helper2')(__filename);
 
 // Write code here!
+
+room.cleanup();
+
+let ill = room.newIllumination()
+ill.fill("red")
+ill.rect(0, 0, 1000, 1000);
+room.draw(ill)
+
+run()
