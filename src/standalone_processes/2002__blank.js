2ca6c7390af9b515586b3ec425857170e0106384
Jacob Haip <haipjacob@gmail.com>
2019-02-04 23:47:45 -0500

    compile #2002 with new parser with pre and postfix subscription

--- src/standalone_processes/2002__blank.js
+++ src/standalone_processes/2002__blank.js
@@ -3,22 +3,38 @@ const { room, myId, run } = require('../helper2')(__filename);
 room.cleanup()
 room.assert(`it is working`)
 
-// new
-
 room.on(`$photon sees color ( $r, $g, $b )`,
         results => {
-if (!results) {
+subscriptionPrefix();
+if (!!results) {
 
     // I'm new
-    room.cleanup()
+    // room.cleanup()
     room.assert(`wish tablet had background color ( 0 , 0 , 0 )`)
 } else {
-    room.cleanup()
+    // room.cleanup()
     let r = results[0].r;
     let g = results[0].g;
     let b = results[0].b;
     room.assert(`wish tablet had background color ( ${r} , ${g} , ${b} )`)
 }
+subscriptionPostfix();
+})
+
+let x = 5;
+
+room.on(`x is ${x}`,
+        `ok`,
+        results => {
+subscriptionPrefix();
+if (!!results) {
+
+  // yo
+} else {
+  // ok
+
+}
+subscriptionPostfix();
 })
 
 
