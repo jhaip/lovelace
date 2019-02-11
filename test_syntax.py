ab55a2b512b44d032a7129b703c4aa3a2c6fcc36
Jacob Haip <haipjacob@gmail.com>
2019-02-10 21:08:46 -0500

    save two scripts for testing syntax parsing

--- test_syntax.py
+++ test_syntax.py
@@ -1,5 +1,39 @@
 import re
 
+def parse_syntax(s):
+    def matchFirst(s, specialParts, r):
+        prog = re.compile(r, re.MULTILINE)
+        for m in prog.finditer(s):
+            return specialParts + [{"type": "when", "index": m.start(),
+                                    "length": len(m.group(1))}]
+    def matchSecond(s, specialParts, r):
+        prog = re.compile(r, re.MULTILINE)
+        for m in prog.finditer(s):
+            return specialParts + [{"type": "otherwise", "index": m.start() + len(m.group(1)),
+                                    "length": len(m.group(2))}]
+    labels = []
+    labels = matchFirst(s, labels, "(when [^:]*:)[\s\S]*otherwise:\n[\s\S]*$")
+    labels = matchSecond(s, labels, "(when [^:]*:[\s\S]*)(otherwise:)\n[\s\S]*$")
+    labels = matchFirst(s, labels, "(when [^:]*:)[\s\S]*end\n")
+    labels = matchSecond(s, labels, "(when [^:]*:[\s\S]*)(end)\n")
+    labels = matchFirst(s, labels, "(when [^:]*:)[\s\S]*$")
+    labels = matchFirst(s, labels, "(claim [^\n]*)")
+    labels = matchFirst(s, labels, "(retract [^\n]*)")
+    labels = matchFirst(s, labels, "(cleanup\n)")
+    labels.sort(key=lambda x: x["index"])
+    head = 0
+    chunks = []
+    for part in labels:
+        if part["index"] >= head:
+            chunks.append({"type": "normal", "text": s[slice(head, part["index"])]})
+            chunks.append({"type": part["type"], "text": s[slice(
+                part["index"], part["index"] + part["length"])]})
+            head = part["index"] + part["length"]
+    if len(chunks) is 0:
+        chunks.append({type: "normal", "text": s})
+    return chunks
+
+
 s = """
 import x;
 
@@ -18,56 +52,5 @@ otherwise:
   // test
 end
 """
+print(parse_syntax(s))
 
-specialParts = []
-
-def matchFirst(prog):
-    for m in prog.finditer(s):
-        specialParts.append({"type": "when", "index": m.start(),
-                            "length": len(m.group(1))})
-
-def matchSecond(prog):
-    for m in prog.finditer(s):
-        specialParts.append({"type": "otherwise", "index": m.start() + len(m.group(1)),
-                            "length": len(m.group(2))})
-
-# whenOtherwiseFunc
-prog = re.compile("(when [^:]*:)[\s\S]*otherwise:\n[\s\S]*$", re.MULTILINE)
-matchFirst(prog)
-prog = re.compile("(when [^:]*:[\s\S]*)(otherwise:)\n[\s\S]*$", re.MULTILINE)
-matchSecond(prog)
-# whenEndFunc
-prog = re.compile("(when [^:]*:)[\s\S]*end\n", re.MULTILINE)
-matchFirst(prog)
-prog = re.compile("(when [^:]*:[\s\S]*)(end)\n", re.MULTILINE)
-matchSecond(prog)
-# whenFunc
-prog = re.compile("(when [^:]*:)[\s\S]*$", re.MULTILINE)
-matchFirst(prog)
-# claimFunc
-prog = re.compile("(claim [^\n]*)", re.MULTILINE)
-matchFirst(prog)
-# retractFunc
-prog = re.compile("(retract [^\n]*)", re.MULTILINE)
-matchFirst(prog)
-# cleanupFunc
-prog = re.compile("(cleanup\n)", re.MULTILINE)
-matchFirst(prog)
-
-print(specialParts)
-specialParts.sort(key=lambda x: x["index"])
-print(specialParts)
-
-head = 0
-chunks = []
-for part in specialParts:
-    if part["index"] >= head:
-        chunks.append({"type": "normal", "text": s[slice(head, part["index"])]})
-        chunks.append({"type": part["type"], "text": s[slice(
-            part["index"], part["index"] + part["length"])]})
-        head = part["index"] + part["length"]
-
-if len(chunks) is 0:
-    chunks.append({type: "normal", "text": s})
-
-print(chunks)
---
+++ test_syntax2.js
@@ -0,0 +1,113 @@
+function parseWithStates(x) {
+    const STATES = { "GLOBAL": "GLOBAL", "WHEN_QUERY_PARAMS": "WHEN_QUERY_PARAMS", "WHEN_TRUE": "WHEN_TRUE", "WHEN_OTHERWISE": "WHEN_OTHERWISE" }
+    let STATE = STATES.GLOBAL;
+    let WHEN_VARIABLES_CACHE = "";
+    let OUTPUT = "";
+
+    const claimFunc = s => {
+        const m = s.match(/^(\s*)claim (.+)\s*$/);
+        if (m === null) return "";
+        return `${m[1]}room.assert(\`${m[2]}\`)\n`;
+    }
+    const retractFunc = s => {
+        const m = s.match(/^(\s*)retract (.+)\s*$/);
+        if (m === null) return "";
+        return `${m[1]}room.retractAll(\`${m[2]}\`)\n`;
+    }
+    const cleanupFunc = s => {
+        const m = s.match(/^(\s*)cleanup\s*$/);
+        if (m === null) return "";
+        return `${m[1]}room.cleanup()\n`;
+    }
+    const getUniqueVariables = s => {
+        const variables = s.match(/\$([a-zA-Z0-9]+)/g);
+        if (variables) {
+            return variables
+                .map(x => x.slice(1))
+                .filter((value, index, self) => self.indexOf(value) === index);
+        }
+        return [];
+    }
+    const lines = x.split("\n")
+    for (let lineIndex = 0; lineIndex < lines.length; lineIndex += 1) {
+        const prevOUTPUT = OUTPUT.slice();
+        const line = lines[lineIndex];
+        const isLastLine = lineIndex === lines.length - 1;
+        if (STATE === STATES.GLOBAL) {
+            OUTPUT += claimFunc(line);
+            OUTPUT += retractFunc(line);
+            OUTPUT += cleanupFunc(line);
+            if (line.match(/^when /)) {
+                STATE = STATES.WHEN_QUERY_PARAMS;
+                if (line.slice(-1) === ':') {
+                    const query = line.match(/^when (.+):$/)[1]
+                    const variables = getUniqueVariables(query);
+                    OUTPUT += `room.on(\`${query}\`,\n`
+                    OUTPUT += `        results => {\n`
+                    OUTPUT += `  subscriptionPrefix();\n`
+                    OUTPUT += `  if (!!results) {\n`
+                    OUTPUT += `    results.forEach(({ ${variables.join(", ")} }) => {\n`
+                    STATE = STATES.WHEN_TRUE;
+                } else if (line.slice(-1) === ',') {
+                    const query = line.match(/^when (.+),$/)[1]
+                    WHEN_VARIABLES_CACHE += ' ' + query;
+                    OUTPUT += `room.on(\`${query}\`,\n`
+                } else {
+                    console.error("bad when query!")
+                }
+            }
+        } else if (STATE == STATES.WHEN_TRUE) {
+            OUTPUT += claimFunc(line);
+            OUTPUT += retractFunc(line);
+            OUTPUT += cleanupFunc(line);
+            if (line.match(/^otherwise:$/)) {
+                STATE = STATES.WHEN_OTHERWISE;
+                OUTPUT += "\n    });\n  } else {\n"
+            }
+            if (line.match(/^end$/) || isLastLine) {
+                STATE = STATES.GLOBAL;
+                OUTPUT += "\n    });\n";
+                OUTPUT += "  }\n  subscriptionPostfix();\n})\n";
+            }
+        } else if (STATE == STATES.WHEN_OTHERWISE) {
+            OUTPUT += claimFunc(line);
+            OUTPUT += retractFunc(line);
+            OUTPUT += cleanupFunc(line);
+            if (line.match(/^end$/) || isLastLine) {
+                STATE = STATES.GLOBAL;
+                OUTPUT += "  }\n  subscriptionPostfix();\n})\n";
+            }
+        } else if (STATE == STATES.WHEN_QUERY_PARAMS) {
+            const m = line.match(/^\s*(.+),$/)
+            if (m) {
+                const query = m[1]
+                WHEN_VARIABLES_CACHE += ' ' + query;
+                OUTPUT += `        \`${query}\`,\n`
+            } else {
+                const m2 = line.match(/^\s*(.+):$/)
+                if (m2) {
+                    const query = m2[1]
+                    WHEN_VARIABLES_CACHE += ' ' + query;
+                    const variables = getUniqueVariables(WHEN_VARIABLES_CACHE)
+                    OUTPUT += `        \`${query}\`,\n`
+                    OUTPUT += `        results => {\n`
+                    OUTPUT += `  subscriptionPrefix();\n`
+                    OUTPUT += `  if (!!results) {\n`
+                    OUTPUT += `    results.forEach(({ ${variables.join(", ")} }) => {\n`
+                    STATE = STATES.WHEN_TRUE;
+                } else {
+                    console.error("BAD QUERY")
+                }
+            }
+        }
+        if (prevOUTPUT === OUTPUT) {
+            OUTPUT += line + "\n";
+        }
+    }
+
+    // end state
+    if (STATE === STATES.WHEN_QUERY_PARAMS) {
+        console.error("NO END TO WHEN QUERY")
+    }
+    return OUTPUT;
+}
