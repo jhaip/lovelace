efd1f0ff33126a40ce4baf54e72db9dd51a14eb8
Jacob Haip <haipjacob@gmail.com>
2019-02-17 10:21:05 -0500

    fix parsing bugs and add first RFID to #45

--- src/standalone_processes/45__runSeenRFID.js
+++ src/standalone_processes/45__runSeenRFID.js
@@ -1,21 +1,21 @@
 const { room, myId, run } = require('../helper2')(__filename);
 
 const rfidValueToPaperId = {
-    'TODO': 2000
+    '80616ea3': 2002
 }
 
 room.on(`$photonId read $value on sensor $sensorId`,
         results => {
-  subscriptionPrefix();
+  room.subscriptionPrefix();
   if (!!results) {
     results.forEach(({ photonId, value, sensorId }) => {
     if (value in rfidValueToPaperId) {
-        room.assert(`camera 99 sees paper ${paperId} at TL (0, 0) TR (1, 0) BR (1, 1) BL (0, 1) @ 1`)
+        room.assert(`camera 99 sees paper ${rfidValueToPaperId[value]} at TL (0, 0) TR (1, 0) BR (1, 1) BL (0, 1) @ 1`)
     }
 
     });
   }
-  subscriptionPostfix();
+  room.subscriptionPostfix();
 })
 
 
