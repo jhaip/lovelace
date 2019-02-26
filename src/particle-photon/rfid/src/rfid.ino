c59c2b2879865d02544ecd7b8d7f21626cd7dedf
Jacob Haip <haipjacob@gmail.com>
2019-02-26 09:07:07 -0500

    update photon code to support 5 RFID readers

--- src/particle-photon/rfid/src/rfid.ino
+++ src/particle-photon/rfid/src/rfid.ino
@@ -33,17 +33,20 @@
 //#include <SPI.h>
 #include "MFRC522.h"
 
-#define SS_PIN D1
 #define RST_PIN D2
 
+#define SS_PIN D1
 #define SS_PIN_B D0
+#define SS_PIN_C D3
+#define SS_PIN_D D4
+#define SS_PIN_E D5
 
 // Create MFRC522 instances
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522 mfrc522_b(SS_PIN_B, RST_PIN);
-// MFRC522 mfrc522_c(SS_PIN_C, RST_PIN);
-// MFRC522 mfrc522_d(SS_PIN_D, RST_PIN);
-// MFRC522 mfrc522_e(SS_PIN_E, RST_PIN);
+MFRC522 mfrc522_c(SS_PIN_C, RST_PIN);
+MFRC522 mfrc522_d(SS_PIN_D, RST_PIN);
+MFRC522 mfrc522_e(SS_PIN_E, RST_PIN);
 // MFRC522 mfrc522_f(SS_PIN_F, RST_PIN);
 
 HttpClient http;
@@ -81,9 +84,15 @@ void setup()
   Serial.begin(9600); // Initialize serial communications with the PC
   mfrc522.setSPIConfig();
   mfrc522_b.setSPIConfig();
+  mfrc522_c.setSPIConfig();
+  mfrc522_d.setSPIConfig();
+  mfrc522_e.setSPIConfig();
 
   mfrc522.PCD_Init();   // Init MFRC522 card
   mfrc522_b.PCD_Init(); // Init MFRC522 card
+  mfrc522_c.PCD_Init(); // Init MFRC522 card
+  mfrc522_d.PCD_Init(); // Init MFRC522 card
+  mfrc522_e.PCD_Init(); // Init MFRC522 card
   Serial.println("Scan PICC to see UID and type...");
 }
 
@@ -124,13 +133,13 @@ void loop()
 
   String val_a = check_reader(mfrc522);
   String val_b = check_reader(mfrc522_b);
-  // String val_c = check_reader(mfrc522_c);
-  // String val_d = check_reader(mfrc522_d);
-  // String val_e = check_reader(mfrc522_e);
+  String val_c = check_reader(mfrc522_c);
+  String val_d = check_reader(mfrc522_d);
+  String val_e = check_reader(mfrc522_e);
   // String val_f = check_reader(mfrc522_f);
 
-  publishValueMessage(1, val_a);
-  publishValueMessage(2, val_b);
+  // publishValueMessage(1, val_a);
+  // publishValueMessage(2, val_b);
   // publishValueMessage(3, val_c);
   // publishValueMessage(4, val_d);
   // publishValueMessage(5, val_e);
