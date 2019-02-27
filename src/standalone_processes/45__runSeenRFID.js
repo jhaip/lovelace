6f095de5d88107d13280d45d3f372526652e6f0f
Jacob Haip <haipjacob@gmail.com>
2019-02-27 00:02:58 -0500

    claim text editor is pointing at papers read by RFID sensor 3

--- src/standalone_processes/45__runSeenRFID.js
+++ src/standalone_processes/45__runSeenRFID.js
@@ -21,7 +21,10 @@ room.on(`$photonId read $value on sensor $sensorId`,
   if (!!results) {
     results.forEach(({ photonId, value, sensorId }) => {
     if (value in rfidValueToPaperId) {
-        room.assert(`camera 99 sees paper ${rfidValueToPaperId[value]} at TL (0, 0) TR (1, 0) BR (1, 1) BL (0, 1) @ 1`)
+        room.assert(`camera 99 sees paper ${rfidValueToPaperId[value]} at TL (0, 0) TR (1, 0) BR (1, 1) BL (0, 1) @ 1`);
+        if (String(sensorId) === String(3)) {
+            room.assert(`paper 1013 is pointing at paper ${rfidValueToPaperId[value]}`);
+        }
     }
 
     });
