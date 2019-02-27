024c7b2357b671beead45919cbd4ed97721471fa
Jacob Haip <haipjacob@gmail.com>
2019-02-26 23:54:44 -0500

    add all RFID papers #2000 - #2009, #5, #6

--- src/standalone_processes/45__runSeenRFID.js
+++ src/standalone_processes/45__runSeenRFID.js
@@ -1,8 +1,18 @@
 const { room, myId, run } = require('../helper2')(__filename);
 
 const rfidValueToPaperId = {
-    '80616ea3': 2002,
-    '10af78a3': 2006
+  'f26a0c2e': 2000,
+  'f238222e': 2001,
+  '80616ea3': 2002,
+  'd07911a3': 2003,
+  '91b4d108': 2004,
+  '53825027': 2005,
+  '10af78a3': 2006,
+  '7341a727': 2007,
+  '2574c72d': 2008,
+  'b680cc21': 2009,
+  'd01ff625': 5,
+  'e21eef27': 6
 }
 
 room.on(`$photonId read $value on sensor $sensorId`,
