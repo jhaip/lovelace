9921ad10a7d687f72b7e13082c08498735ab564e
Jacob Haip <haipjacob@gmail.com>
2019-02-27 22:08:05 -0500

    recompile #45

--- src/standalone_processes/45__runSeenRFID.js
+++ src/standalone_processes/45__runSeenRFID.js
@@ -16,22 +16,22 @@ const rfidValueToPaperId = {
 }
 
 const PAPER_WIDTH = 200
-const PAPER_HEIGHT = 300
-const PAPER_H_MARGIN = 45
+const PAPER_HEIGHT = 350
+const PAPER_H_MARGIN = 20
 const PAPER_V_MARGIN = 20
-const ORIGIN_X = 590
-const ORIGIN_Y = 40
+const ORIGIN_X = 400
+const ORIGIN_Y = 900
 const x1 = x => ORIGIN_X + x * (PAPER_HEIGHT + PAPER_V_MARGIN);
 const x2 = x => ORIGIN_X + x * (PAPER_HEIGHT + PAPER_V_MARGIN) + PAPER_HEIGHT;
 const y1 = y => ORIGIN_Y + y * (PAPER_WIDTH + PAPER_H_MARGIN);
 const y2 = y => ORIGIN_Y + y * (PAPER_WIDTH + PAPER_H_MARGIN) + PAPER_WIDTH;
 const Z = (x, y) => `TL (${x1(x)}, ${y1(y)}) TR (${x2(x)}, ${y1(y)}) BR (${x2(x)}, ${y2(y)}) BL (${x1(x)}, ${y2(y)})`
 const sensorScreenLocations = {
-  '1': Z(0, 0),
+  '1': Z(1, 1),
   '2': Z(1, 0),
   '3': 'TL (0, 0) TR (1, 0) BR (1, 1) BL (0, 1)',
-  '4': Z(0, 1),
-  '5': Z(1, 1)
+  '4': Z(0, 0),
+  '5': Z(0, 1)
 }
 
 room.on(`$photonId read $value on sensor $sensorId`,
