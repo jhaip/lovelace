2244caeb21600116d7f4c4fad208e6fbda798c7f
Jacob Haip <haipjacob@gmail.com>
2019-02-27 21:42:46 -0500

    have #45 claim RFID sensed paper locations & handle editor sensing better

--- src/standalone_processes/45__runSeenRFID.js
+++ src/standalone_processes/45__runSeenRFID.js
@@ -15,22 +15,51 @@ const rfidValueToPaperId = {
   'e21eef27': 6
 }
 
+const PAPER_WIDTH = 200
+const PAPER_HEIGHT = 300
+const PAPER_H_MARGIN = 45
+const PAPER_V_MARGIN = 20
+const ORIGIN_X = 590
+const ORIGIN_Y = 40
+const x1 = x => ORIGIN_X + x * (PAPER_HEIGHT + PAPER_V_MARGIN);
+const x2 = x => ORIGIN_X + x * (PAPER_HEIGHT + PAPER_V_MARGIN) + PAPER_HEIGHT;
+const y1 = y => ORIGIN_Y + y * (PAPER_WIDTH + PAPER_H_MARGIN);
+const y2 = y => ORIGIN_Y + y * (PAPER_WIDTH + PAPER_H_MARGIN) + PAPER_WIDTH;
+const Z = (x, y) => `TL (${x1(x)}, ${y1(y)}) TR (${x2(x)}, ${y1(y)}) BR (${x2(x)}, ${y2(y)}) BL (${x1(x)}, ${y2(y)})`
+const sensorScreenLocations = {
+  '1': Z(0, 0),
+  '2': Z(1, 0),
+  '3': 'TL (0, 0) TR (1, 0) BR (1, 1) BL (0, 1)',
+  '4': Z(0, 1),
+  '5': Z(1, 1)
+}
+
 room.on(`$photonId read $value on sensor $sensorId`,
         results => {
   room.subscriptionPrefix(1);
   if (!!results) {
     results.forEach(({ photonId, value, sensorId }) => {
     if (value in rfidValueToPaperId) {
-        room.assert(`camera 99 sees paper ${rfidValueToPaperId[value]} at TL (0, 0) TR (1, 0) BR (1, 1) BL (0, 1) @ 1`);
-        if (String(sensorId) === String(3)) {
-            room.assert(`paper 1013 is pointing at paper ${rfidValueToPaperId[value]}`);
-        }
+        room.assert(`camera 1 sees paper ${rfidValueToPaperId[value]} at ${sensorScreenLocations[sensorId]} @ 1`);
     }
 
     });
   }
   room.subscriptionPostfix();
 })
+
+room.on(`$photonId read $value on sensor $sensorId`,
+  results => {
+    room.subscriptionPrefix(1);
+    if (!!results) {
+      results.forEach(({ photonId, value, sensorId }) => {
+        if (value in rfidValueToPaperId) {
+          room.assert(`paper 1013 is pointing at paper ${rfidValueToPaperId[value]}`);
+        }
+      });
+    }
+    room.subscriptionPostfix();
+  })
 
 
 run();
