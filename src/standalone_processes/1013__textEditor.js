3bd5afa6039f2675aee4393866802b32537ab840
Jacob Haip <haipjacob@gmail.com>
2019-03-02 00:17:23 -0500

    comment out dots drawing in #1382 print paper, for now

--- src/standalone_processes/1013__textEditor.js
+++ src/standalone_processes/1013__textEditor.js
@@ -280,8 +280,7 @@ room.on(
           `with source code`, ["text", cleanSourceCode],
           `@ ${millis}`);
       } else if (specialKey === "C-p") {
-        // room.assert(`wish paper ${currentTargetId} at`, ["text", currentTargetName], `would be printed`)
-        room.assert(`wish file`, ["text", `${__dirname}/${currentTargetName}`], `would be printed`) // no dots
+        room.assert(`wish paper ${currentTargetId} at`, ["text", currentTargetName], `would be printed`)
       } else if (specialKey === "C-+") {
         fontSize += 2;
         render();
