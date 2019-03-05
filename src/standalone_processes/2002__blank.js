dc5257c2236713da9f7bee580a7b03ba5a9ad1d3
Jacob Haip <haipjacob@gmail.com>
2019-03-05 09:34:34 -0500

    save the updated #2002 with now flashes a blink program to photons

--- src/standalone_processes/2002__blank.js
+++ src/standalone_processes/2002__blank.js
@@ -1,38 +1,18 @@
 const { room, myId, run } = require('../helper2')(__filename);
 
-room.cleanup()
-room.assert(`new it is working again`)
+const code = `
+void setup() {
+  pinMode(D7, OUTPUT);
+}
 
-room.on(`$photon sees color ( $r, $g, $b )`,
-        results => {
-  room.subscriptionPrefix(1);
-  if (!!results) {
-    results.forEach(({ photon, r, g, b }) => {
-    room.assert(`wish tablet had background color ( ${r} , ${g} , ${b} )`)
-
-    });
-  } else {
-    room.assert(`wish tablet had background color ( 0 , 0 , 0 )`)
-  }
-  room.subscriptionPostfix();
-})
-
-let x = 5;
-
-room.on(`x is ${x}`,
-        `ok`,
-        results => {
-  room.subscriptionPrefix(2);
-  if (!!results) {
-    results.forEach(({  }) => {
-  // yo
-
-    });
-  } else {
-  // ok
-  }
-  subscriptionPostfix();
-})
+void loop() {
+  delay(1000);
+  digitalWrite(D7, HIGH);
+  delay(1000);
+  digitalWrite(D7, LOW);
+}
+`
+room.assert(`wish`, ["text", code], `runs on the photon`)
 
 
 run();
