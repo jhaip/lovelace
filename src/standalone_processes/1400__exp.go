734b7fde681a40780537645fb60d32285b62e52f
Jacob Haip <haipjacob@gmail.com>
2019-05-18 14:20:06 -0400

    first working test of go ping pong + fix jig

--- src/standalone_processes/1400__exp.go
+++ src/standalone_processes/1400__exp.go
@@ -22,11 +22,15 @@ func main() {
 	MY_ID := os.Args[1]
 	F, _ := strconv.Atoi(MY_ID)
 	MY_ID_STR := fmt.Sprintf("%04d", F)
-  F = F + N
+	if F == 1400 {
+		F = F + N
+	} else {
+		F = F + 1
+	}
 	SUBSCRIPTION_ID_LEN := 36
-	fmt.Println(MY_ID)
-	fmt.Println(F)
-	fmt.Println(MY_ID_STR)
+	// fmt.Println(MY_ID)
+	// fmt.Println(F)
+	// fmt.Println(MY_ID_STR)
 
 	publisher, _ := zmq.NewSocket(zmq.PUB)
 	defer publisher.Close()
@@ -40,21 +44,23 @@ func main() {
 	source_len := 4
 	server_send_time_len := 13
 
-	time.Sleep(100.0 * time.Millisecond)
+	startTimeMs := time.Now().UnixNano() / 1000000
+
+	time.Sleep(10.0 * time.Millisecond)
 
 	publisher.Send(fmt.Sprintf(".....PING%s%s", MY_ID_STR, init_ping_id), zmq.DONTWAIT)
-	fmt.Println("sent ping")
+	// fmt.Println("sent ping")
 
 	for {
 		msg, _ := subscriber.Recv(0)
-		fmt.Println("Recv")
-		fmt.Println(msg)
+		// fmt.Println("Recv")
+		// fmt.Println(msg)
 		id := msg[source_len:(source_len + SUBSCRIPTION_ID_LEN)]
-		fmt.Println("ID:")
-		fmt.Println(id)
+		// fmt.Println("ID:")
+		// fmt.Println(id)
 		val := msg[(source_len + SUBSCRIPTION_ID_LEN + server_send_time_len):]
 		if id == init_ping_id {
-			fmt.Println("server is listening")
+			// fmt.Println("server is listening")
 			break
 		} else {
 			fmt.Println(val)
@@ -62,28 +68,38 @@ func main() {
 	}
 
 	subscription_id := RandomString(SUBSCRIPTION_ID_LEN)
-	sub_msg := fmt.Sprintf("{\"id\": \"%s\", \"facts\": [\"$ test client %s says $x @ $time1\", \"$ test client %d says $y @ $time2\"]}", subscription_id, MY_ID, F)
+	// sub_msg := fmt.Sprintf("{\"id\": \"%s\", \"facts\": [\"$ test client %s says $x @ $time1\", \"$ test client %d says $y @ $time2\"]}", subscription_id, MY_ID, F)
+	sub_msg := fmt.Sprintf("{\"id\": \"%s\", \"facts\": [\"$ test client %d says $y @ $time2\"]}", subscription_id, F)
 	publisher.Send(fmt.Sprintf("SUBSCRIBE%s%s", MY_ID_STR, sub_msg), zmq.DONTWAIT)
-	fmt.Println("subscribe done")
+	// fmt.Println("subscribe done")
 
-	time.Sleep(100.0 * time.Millisecond)
+	// time.Sleep(10.0 * time.Millisecond)
 	
 	currentTimeMs := time.Now().UnixNano() / 1000000
 	claim_msg := fmt.Sprintf("[{\"type\": \"claim\", \"fact\": [[\"text\", \"%s\"], [\"text\", \"test\"], [\"text\", \"client\"], [\"integer\", \"%s\"], [\"text\", \"says\"], [\"integer\", \"%s\"], [\"text\", \"@\"], [\"integer\", \"%d\"]]}]", MY_ID_STR, MY_ID, MY_ID, currentTimeMs)
 	publisher.Send(fmt.Sprintf("....BATCH%s%s", MY_ID_STR, claim_msg), zmq.DONTWAIT)
-	fmt.Println("startup claim done")
+	// fmt.Println("startup claim done")
 		
 	for {
-		msg, _ := subscriber.Recv(0)
-		fmt.Println("Recv")
-		fmt.Println(msg)
-		id := msg[source_len:(source_len + SUBSCRIPTION_ID_LEN)]
-		val := msg[(source_len + SUBSCRIPTION_ID_LEN + server_send_time_len):]
-		if id == init_ping_id {
-			fmt.Println("server is listening")
-			fmt.Println(id)
+		subscriber.Recv(0)
+		// msg, _ := subscriber.Recv(0)
+		// fmt.Println(fmt.Sprintf("Recv %s", MY_ID_STR))
+		// fmt.Println(msg)
+		// id := msg[source_len:(source_len + SUBSCRIPTION_ID_LEN)]
+		// val := msg[(source_len + SUBSCRIPTION_ID_LEN + server_send_time_len):]
+		// fmt.Println(id)
+		// fmt.Println(val)
+
+		currentTimeMs := time.Now().UnixNano() / 1000000
+		if MY_ID == "1400" {
+			// finish_time := msg[22:35]  // hack to extract time instead of parsing JSON
+			fmt.Println("test is done!")
+			fmt.Println(fmt.Sprintf("elapsed time: %d ms", currentTimeMs-startTimeMs))
 		} else {
-			fmt.Println(val)
+			
+			claim_msg := fmt.Sprintf("[{\"type\": \"claim\", \"fact\": [[\"text\", \"%s\"], [\"text\", \"test\"], [\"text\", \"client\"], [\"integer\", \"%s\"], [\"text\", \"says\"], [\"integer\", \"%s\"], [\"text\", \"@\"], [\"integer\", \"%d\"]]}]", MY_ID_STR, MY_ID, MY_ID, currentTimeMs)
+			publisher.Send(fmt.Sprintf("....BATCH%s%s", MY_ID_STR, claim_msg), zmq.DONTWAIT)
+			// fmt.Println("sub recv claim done")
 		}
 		break
 	}
