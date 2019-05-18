65ecbe350c97550de2ce580c0fdf72bdc19ea506
Jacob Haip <haipjacob@gmail.com>
2019-05-18 13:12:09 -0400

    fix the test5_zmq_go ping pong

--- new-backend/test5_zmq_go_pong.go
+++ new-backend/test5_zmq_go_pong.go
@@ -7,27 +7,28 @@ import (
   "os"
 )
 
-var N int = 1000
+var N int = 10
 var i int = N
 
 func main() {
   // fmt.Println("Connecting to hello world server...")
   publisher, _ := zmq.NewSocket(zmq.PUB)
   defer publisher.Close()
-  publisher.Connect("ipc://test-pub-sub-5555.ipc")
+  publisher.Connect("ipc://test-pub-sub-5556.ipc")
   subscriber, _ := zmq.NewSocket(zmq.SUB)
 	defer subscriber.Close()
-  subscriber.Connect("ipc://test-pub-sub-5556.ipc")
+  subscriber.Connect("ipc://test-pub-sub-555.ipc")
   subscriber.SetSubscribe("SUBSCRIBE")
 
   time.Sleep(1.0 * time.Second)
   start := time.Now()
   publisher.Send("....CLAIM5TEST", zmq.DONTWAIT)
+  fmt.Printf("sent")
 
 	for {
-		// msg, _ := subscriber.Recv(0)
-    subscriber.Recv(0)
-		// fmt.Printf("%s\n", msg)
+		msg, _ := subscriber.Recv(0)
+    // subscriber.Recv(0)
+		fmt.Printf("%s\n", msg)
     publisher.Send("....CLAIM5TEST", zmq.DONTWAIT)
     i -= 1
     // fmt.Printf("%s %v\n", msg, i)
