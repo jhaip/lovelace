bc36f69ad261d7756a47b279c79b70ced8a8708b
Jacob Haip <haipjacob@gmail.com>
2019-06-26 00:41:01 -0400

    make #648 wait for events instead of constantly polling and taking up a lot of CPU

--- src/standalone_processes/648__keyboard.py
+++ src/standalone_processes/648__keyboard.py
@@ -49,38 +49,40 @@ def add_key(key, special_key):
 
 pygame.init()
 screen = pygame.display.set_mode((50, 50), 0, 32)
+clock = pygame.time.Clock()
 while True:
+    # clock.tick(60)  # prevent loop from going faster than 60fps
+    event = pygame.event.wait()
     pressed = pygame.key.get_pressed()
     ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE:
-                add_key(None, "backspace")
-            elif event.key == pygame.K_RETURN:
-                add_key(None, "enter")
-            elif event.key == pygame.K_TAB:
-                add_key(None, "tab")
-            elif event.key == pygame.K_SPACE:
-                add_key(None, "space")
-            elif event.key == pygame.K_LEFT:
-                add_key(None, "left")
-            elif event.key == pygame.K_RIGHT:
-                add_key(None, "right")
-            elif event.key == pygame.K_UP:
-                add_key(None, "up")
-            elif event.key == pygame.K_DOWN:
-                add_key(None, "down")
-            elif event.key == pygame.K_n and ctrl_held:
-                add_key(None, "C-n")
-            elif event.key == pygame.K_p and ctrl_held:
-                add_key(None, "C-p")
-            elif event.key == pygame.K_s and ctrl_held:
-                add_key(None, "C-s")
-            elif event.key == pygame.K_EQUALS and ctrl_held:
-                add_key(None, "C-+")
-            elif event.key == pygame.K_MINUS and ctrl_held:
-                add_key(None, "C--")
-            elif event.key == pygame.K_QUOTEDBL:
-                add_key(None, "\"")
-            elif len(event.unicode) > 0:
-                add_key(event.unicode, None)
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_BACKSPACE:
+            add_key(None, "backspace")
+        elif event.key == pygame.K_RETURN:
+            add_key(None, "enter")
+        elif event.key == pygame.K_TAB:
+            add_key(None, "tab")
+        elif event.key == pygame.K_SPACE:
+            add_key(None, "space")
+        elif event.key == pygame.K_LEFT:
+            add_key(None, "left")
+        elif event.key == pygame.K_RIGHT:
+            add_key(None, "right")
+        elif event.key == pygame.K_UP:
+            add_key(None, "up")
+        elif event.key == pygame.K_DOWN:
+            add_key(None, "down")
+        elif event.key == pygame.K_n and ctrl_held:
+            add_key(None, "C-n")
+        elif event.key == pygame.K_p and ctrl_held:
+            add_key(None, "C-p")
+        elif event.key == pygame.K_s and ctrl_held:
+            add_key(None, "C-s")
+        elif event.key == pygame.K_EQUALS and ctrl_held:
+            add_key(None, "C-+")
+        elif event.key == pygame.K_MINUS and ctrl_held:
+            add_key(None, "C--")
+        elif event.key == pygame.K_QUOTEDBL:
+            add_key(None, "\"")
+        elif len(event.unicode) > 0:
+            add_key(event.unicode, None)
