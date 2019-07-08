b09bd8213b7252f76d74a5bed48e086fb43fc05e
Jacob Haip <haipjacob@gmail.com>
2019-07-07 23:38:08 -0400

    refactor 1900 to use async pkill for start

--- src/standalone_processes/1900__processManager.js
+++ src/standalone_processes/1900__processManager.js
@@ -9,49 +9,47 @@ let nameToProcessIdCache = {};
 function runPaper(name) {
   console.error(`making ${name} be running!`)
   // kill any old processes that weren't correctly killed before
-  if (name !== '1900__processManager.js') {
-    try {
-      pkill.full(`${name}`)
-    } catch {
+  pkill.full(`${name}`, function (err, validPid) {
+    if (err) {
       console.error("ERROR PKILLING", name)
+      return;
     }
-  } else {
-    console.error("skip killing 1900 itself")
-  }
-  let languageProcess = 'node'
-  let programSource = `src/standalone_processes/${name}`
-  let runArgs = [programSource];
-  if (name.includes('.py')) {
-    languageProcess = 'python3'
-  } else if (name.includes('.go')) {
-    languageProcess = 'go'
-    runArgs = ['run', programSource]
-  }
-  const child = spawn(languageProcess, runArgs)
-  // child.stdout.on('data', (data) => {
-  //   console.error(`stdout for ${name}`);
-  //   console.log(`stdout: ${data}`);
-  // });
-  // child.stderr.on('data', (data) => {
-  //   console.error(`stderr for ${name}`);
-  //   console.log(`stderr: ${data}`);
-  // });
-  child.on('close', (code) => {
-    // TODO: check if program should still be running
-    // and start it again if so.
-    console.error("program died:")
-    console.error(name);
-    console.error([["id", MY_ID_STR], ["text", name], `has process id $`])
-    room.retractMine(["text", name], `has process id $`);
-    delete nameToProcessIdCache[name];
-    room.flush();
-    const dyingPaperIdString = getIdStringFromId(getIdFromProcessName(name))
-    room.cleanupOtherSource(dyingPaperIdString)
-  });
-  const pid = child.pid;
-  room.assert(["text", name], `has process id ${pid}`);
-  nameToProcessIdCache[name] = pid;
-  console.error(pid);
+    console.error(`kill ${validPid} old processes with the name "${name}"`)
+    let languageProcess = 'node'
+    let programSource = `src/standalone_processes/${name}`
+    let runArgs = [programSource];
+    if (name.includes('.py')) {
+      languageProcess = 'python3'
+    } else if (name.includes('.go')) {
+      languageProcess = 'go'
+      runArgs = ['run', programSource]
+    }
+    const child = spawn(languageProcess, runArgs)
+    // child.stdout.on('data', (data) => {
+    //   console.error(`stdout for ${name}`);
+    //   console.log(`stdout: ${data}`);
+    // });
+    // child.stderr.on('data', (data) => {
+    //   console.error(`stderr for ${name}`);
+    //   console.log(`stderr: ${data}`);
+    // });
+    child.on('close', (code) => {
+      // TODO: check if program should still be running
+      // and start it again if so.
+      console.error("program died:")
+      console.error(name);
+      console.error([["id", MY_ID_STR], ["text", name], `has process id $`])
+      room.retractMine(["text", name], `has process id $`);
+      delete nameToProcessIdCache[name];
+      room.flush();
+      const dyingPaperIdString = getIdStringFromId(getIdFromProcessName(name))
+      room.cleanupOtherSource(dyingPaperIdString)
+    });
+    const pid = child.pid;
+    room.assert(["text", name], `has process id ${pid}`);
+    nameToProcessIdCache[name] = pid;
+    console.error(pid);
+  })
 }
 
 function stopPaper(name, pid) {
