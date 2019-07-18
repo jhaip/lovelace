69951489b3f36e87280b7acf176998d63a3c88b3
Jacob Haip <haipjacob@gmail.com>
2019-07-18 00:29:58 -0400

    reduce default font size

--- src/standalone_processes/1013__textEditor.js
+++ src/standalone_processes/1013__textEditor.js
@@ -1,6 +1,6 @@
 const { room, myId, MY_ID_STR } = require('../helper2')(__filename);
 
-let fontSize = 32; // 12;
+let fontSize = 18;
 const origin = [0, 0]
 let cursorPosition = [0, 0]
 let currentWidth = 1;
