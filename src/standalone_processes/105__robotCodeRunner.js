e58b7d8e01d4ffb8ce06e4a9bf6518f47e71e1c4
Jacob Haip <haipjacob@gmail.com>
2019-07-26 22:59:49 -0400

    add robot running commands: jig, code runner, and example code

---
+++ src/standalone_processes/105__robotCodeRunner.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { room, myId, scriptName, run } = require('../helper2')(__filename);
+
+room.onGetSource('wisherId',
+    `wish $sourceCode runs on robot`,
+    results => {
+        room.subscriptionPrefix(1);
+        if (!!results) {
+            results.forEach(({ time }) => {
+                const currentTargetName = '106__robotCode.py';
+                room.assert(
+                    `wish`, ["text", currentTargetName],
+                    `has source code`, ["text", cleanSourceCode]);
+                // claim it's not running to force the paper to be killed
+                // so the source code change is used when it starts again
+                let postCompiledTargetName = currentTargetName.replace(".prejs", ".js");
+                room.retractAll(
+                    `wish`, ["text", postCompiledTargetName], `would be running`
+                )
+                setTimeout(() => {
+                    room.assert(`wish`, ["text", postCompiledTargetName], `would be running`)
+                    room.flush();
+                }, 2000);
+            });
+        }
+        room.subscriptionPostfix();
+    }
+);
