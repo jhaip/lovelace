4eecc8eb8c94a28e5a206246e79a91012aea7f96
Jacob Haip <haipjacob@gmail.com>
2019-08-06 21:08:35 -0400

    update 989 to persist multiple calibrations and clean up the file

--- src/standalone_processes/989__persistProjectorCalibration.js
+++ src/standalone_processes/989__persistProjectorCalibration.js
@@ -5,26 +5,30 @@ const savedCalibrationLocation = __filename.replace(scriptName, 'files/projector
 
 fs.readFile(savedCalibrationLocation, 'utf8', function(err, contents) {
   if (err) {
-    room.assert(`camera 1 has projector calibration TL (0, 0) TR (1920, 0) BR (1920, 1080) BL (0, 1080) @ 1`)
-    console.log("claiming default calibration because save file didn't exist")
+    console.log("No saved calibration data")
     console.error(err);
   } else {
     console.log("loaded initial calibration:")
     console.log(contents);
-    room.assert(contents);
+    contents.split(/\r?\n/).forEach(line => {
+      room.assert(contents);
+    });
   }
   // Listen for calibration updates and save them
   console.log("listening for changes to calibration")
-  room.onGetSource('wisherId',
-    `camera 1 has projector calibration TL ($x1, $y1) TR ($x2, $y2) BR ($x3, $y3) BL ($x4, $y4) @ $`,
+  room.on(`camera $cameraId has projector calibration TL ($x1, $y1) TR ($x2, $y2) BR ($x3, $y3) BL ($x4, $y4) @ $`,
     results => {
-      results.forEach(({ wisherId, cameraId, x1, y1, x2, y2, x3, y3, x4, y4 }) => {
-        const s = `camera ${cameraId || 0} has projector calibration TL (${x1 || 0}, ${y1 || 0}) TR (${x2 || 0}, ${y2 || 0}) BR (${x3 || 0}, ${y3 || 0}) BL (${x4 || 0}, ${y4 || 0}) @ 1`
-        fs.writeFile(savedCalibrationLocation, s, function(err) {
+      if (!!results) {
+        let serializedData = "";
+        results.forEach(({ cameraId, x1, y1, x2, y2, x3, y3, x4, y4 }) => {
+          serializedData += `camera ${cameraId} has projector calibration TL (${x1}, ${y1}) TR (${x2}, ${y2}) BR (${x3}, ${y3}) BL (${x4}, ${y4}) @ 1`;
+          serializedData += "\n";
+        });
+        fs.writeFile(savedCalibrationLocation, serializedData, function (err) {
           if (err) return console.log(err)
           console.log("The file was saved!");
         });
-      })
+      }
     }
   );
   run();
