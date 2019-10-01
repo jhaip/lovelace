f5e3a1a99eec4970d4993c067ead0a87341f30df
Jacob Haip <haipjacob@gmail.com>
2019-09-30 21:49:53 -0400

    add a debug program to log the text from program 910 "make laser region"

---
+++ src/standalone_processes/911__showMakeRegionStatus.js
@@ -0,0 +1,19 @@
+const { room, myId, run } = require('../helper2')(__filename);
+
+room.onRaw(`#0910 $ draw graphics $graphics on $`,
+    results => {
+        room.subscriptionPrefix(1);
+        if (!!results) {
+            results.forEach(({ graphics }) => {
+                let parsedGraphics = JSON.parse(graphics)
+                parsedGraphics.forEach(g => {
+                    if (g["type"] === "text") {
+                        console.log(g["text"])
+                    }
+                })
+            });
+        }
+        room.subscriptionPostfix();
+    })
+
+run();
