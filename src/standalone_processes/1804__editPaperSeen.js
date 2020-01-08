a2730570d9acf6595a980fee66691eea26ae0679
Jacob Haip <haipjacob@gmail.com>
2020-01-07 22:34:21 -0500

    add start for editor computer

---
+++ src/standalone_processes/1804__editPaperSeen.js
@@ -0,0 +1,16 @@
+const { room, myId, run } = require('../helper2')(__filename);
+
+room.on(`camera $ sees paper $id at TL ($x1, $y1) TR ($x2, $y2) BR ($x3, $y3) BL ($x4, $y4) @ $time`,
+    results => {
+        room.subscriptionPrefix(1);
+        if (!!results) {
+            results.forEach(({ id }) => {
+                room.assert(`paper 1013 is pointing at paper ${id}`)
+                room.assert(`paper ${id} has width 1920 height 1080 angle 0 at ( 0 , 0 )`)
+            });
+        }
+        room.subscriptionPostfix();
+    })
+
+
+run();
