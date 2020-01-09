1dafcca28faf419b7ff789335d591bf7d209a962
Jacob Haip <haipjacob@gmail.com>
2020-01-08 21:08:57 -0500

    claim claiming that 1013 is seen as well

--- src/standalone_processes/1804__editPaperSeen.js
+++ src/standalone_processes/1804__editPaperSeen.js
@@ -5,8 +5,9 @@ room.onRaw(`#1800 $ camera $ sees paper $id at TL ($x1, $y1) TR ($x2, $y2) BR ($
         room.subscriptionPrefix(1);
         if (!!results) {
             results.forEach(({ id }) => {
-                room.assert(`paper 1013 is pointing at paper ${id}`)
+                room.assert(`camera 99 sees paper 1013 at TL( 0 , 0 ) TR ( 1920 , 0 ) BR ( 1920 , 1080 ) BL ( 0 , 1080 ) @ 1`)
                 room.assert(`paper 1013 has width 1920 height 1080 angle 0 at ( 0 , 0 )`)
+                room.assert(`paper 1013 is pointing at paper ${id}`)
             });
         }
         room.subscriptionPostfix();
