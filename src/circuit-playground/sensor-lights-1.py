234512def95954017e29a9a15fe766fec3108297
Jacob Haip <haipjacob@gmail.com>
2020-01-19 15:59:12 -0500

    add circuit playground input and outputs

---
+++ src/circuit-playground/sensor-lights-1.py
@@ -0,0 +1,46 @@
+import time
+import board
+import busio
+from adafruit_circuitplayground.express import cpx
+
+N_PIXELS = 10
+pixels = neopixel.NeoPixel(board.NEOPIXEL, N_PIXELS, brightness=0.2, auto_write=False)
+uart = busio.UART(board.TX, board.RX, baudrate=115200)
+last_sensor_reading = time.monotonic()
+
+button_a = digitalio.DigitalInOut(board.BUTTON_A)
+button_a.switch_to_input(pull=digitalio.Pull.DOWN)
+button_b = digitalio.DigitalInOut(board.BUTTON_A)
+button_b.switch_to_input(pull=digitalio.Pull.DOWN)
+light = analogio.AnalogIn(board.LIGHT)
+
+while True:
+    # Read commands to change outputs
+    data = uart.readline()
+    if data is not None:
+        parsed_data = data.rstrip().split(b",")
+        if len(parsed_data) > 1:
+            prefix = parsed_data[0]
+            if prefix == b'LIGHT' and len(parsed_data) == 5:
+                print("PARSING LIGHT COMMAND: {}".format(data))
+                pixel_id = int(parsed_data[1])
+                if pixel_id >= 0 and parsed_data < N_PIXELS:
+                    r = int(parsed_data[2])
+                    g = int(parsed_data[3])
+                    b = int(parsed_data[4])
+                    pixels[pixel_id] = (r, g, b)
+                    pixels.show()
+    
+    # Write sensor values
+    if time.monotonic() - last_sensor_reading > 1000:
+        last_sensor_reading = time.monotonic()
+        button_a_value = if button_a.value then 1 else 0
+        button_b_value = if button_b.value then 1 else 0
+        light_value = light.value
+        print("SENDING SENSOR VALUES: {} {} {}".format(button_a_value, button_b_value, light_value))
+        uart.write(b"BUTTON_A:{}\n".format(button_a_value))
+        uart.write(b"BUTTON_B:{}\n".format(button_b_value))
+        uart.write(b"LIGHT:{}\n".format(light_value))
+
+    time.sleep(0.1)
+    
---
+++ src/standalone_processes/1192__circuitPythonProxy.py
@@ -0,0 +1,56 @@
+from helper2 import init, claim, retract, prehook, subscription, batch, MY_ID_STR, listen
+import serial
+import logging
+import time
+
+write_buffer = []
+
+@subscription(["$ $ wish circuit playground neopixel $i had color $r $g $b"])
+def melody_callback(results):
+    global write_buffer
+    # b"LIGHT,0,255,128,0\n"
+    if results:
+        for result in results:
+            write_buffer.append(b"LIGHT,0,255,128,0\n".format(
+                result["i"], result["r"], result["g"], result["b"]))
+
+
+init(__file__, skipListening=True)
+
+
+with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
+    received_msg = True
+    while received_msg:
+        received_msg = listen(blocking=False)
+    # Send new messages if there are any
+    if len(write_buffer) > 0:
+        for line in write_buffer:
+            ser.write(line)
+        write_buffer = []
+    # Receive serial messages
+    claims = [
+        {"type": "retract", "fact": [["id", get_my_id_str()], ["id", "0"], ["postfix", ""]]}
+    ]
+    lines = ser.readlines()  # used the serial timeout specified above
+    for line in lines:
+        # Example: line = b'BUTTON_A:1\n'
+        parsed_line = line.rstrip().split(b":")
+        if len(parsed_line) is 2:
+            prefix = parsed_line[0].decode("utf-8")
+            value = parsed_line[1]
+            if prefix == 'BUTTON_A' or prefix == 'BUTTON_B' or prefix == 'LIGHT_SENSOR':
+                claims.append({"type": "claim", "fact": [
+                    ["id", get_my_id_str()],
+                    ["id", "0"],
+                    ["text", "circuit"],
+                    ["text", "playground"],
+                    ["text", prefix],
+                    ["text", "has"],
+                    ["text", "value"],
+                    ["integer", value.decode("utf-8")],
+                ]})
+            else:
+                logging.info("Ignoring message: {}".format(line))
+    if claims:
+        batch(claims)
+    time.sleep(0.1)
