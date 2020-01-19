078171ada5bb9ba60b63dbdf4bb9bd5d04f0d2c7
Jacob Haip <haipjacob@gmail.com>
2020-01-19 16:10:33 -0500

    fixes for 1192 circuit python proxy

--- src/standalone_processes/1192__circuitPythonProxy.py
+++ src/standalone_processes/1192__circuitPythonProxy.py
@@ -1,4 +1,4 @@
-from helper2 import init, claim, retract, prehook, subscription, batch, MY_ID_STR, listen
+from helper2 import init, claim, retract, prehook, subscription, batch, MY_ID_STR, listen, get_my_id_str
 import serial
 import logging
 import time
@@ -19,38 +19,39 @@ init(__file__, skipListening=True)
 
 
 with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
-    received_msg = True
-    while received_msg:
-        received_msg = listen(blocking=False)
-    # Send new messages if there are any
-    if len(write_buffer) > 0:
-        for line in write_buffer:
-            ser.write(line)
-        write_buffer = []
-    # Receive serial messages
-    claims = [
-        {"type": "retract", "fact": [["id", get_my_id_str()], ["id", "0"], ["postfix", ""]]}
-    ]
-    lines = ser.readlines()  # used the serial timeout specified above
-    for line in lines:
-        # Example: line = b'BUTTON_A:1\n'
-        parsed_line = line.rstrip().split(b":")
-        if len(parsed_line) is 2:
-            prefix = parsed_line[0].decode("utf-8")
-            value = parsed_line[1]
-            if prefix == 'BUTTON_A' or prefix == 'BUTTON_B' or prefix == 'LIGHT_SENSOR':
-                claims.append({"type": "claim", "fact": [
-                    ["id", get_my_id_str()],
-                    ["id", "0"],
-                    ["text", "circuit"],
-                    ["text", "playground"],
-                    ["text", prefix],
-                    ["text", "has"],
-                    ["text", "value"],
-                    ["integer", value.decode("utf-8")],
-                ]})
-            else:
-                logging.info("Ignoring message: {}".format(line))
-    if claims:
-        batch(claims)
-    time.sleep(0.1)
+    while True:
+        received_msg = True
+        while received_msg:
+            received_msg = listen(blocking=False)
+        # Send new messages if there are any
+        if len(write_buffer) > 0:
+            for line in write_buffer:
+                ser.write(line)
+            write_buffer = []
+        # Receive serial messages
+        claims = [
+            {"type": "retract", "fact": [["id", get_my_id_str()], ["id", "0"], ["postfix", ""]]}
+        ]
+        lines = ser.readlines()  # used the serial timeout specified above
+        for line in lines:
+            # Example: line = b'BUTTON_A:1\n'
+            parsed_line = line.rstrip().split(b":")
+            if len(parsed_line) is 2:
+                prefix = parsed_line[0].decode("utf-8")
+                value = parsed_line[1]
+                if prefix == 'BUTTON_A' or prefix == 'BUTTON_B' or prefix == 'LIGHT_SENSOR':
+                    claims.append({"type": "claim", "fact": [
+                        ["id", get_my_id_str()],
+                        ["id", "0"],
+                        ["text", "circuit"],
+                        ["text", "playground"],
+                        ["text", prefix],
+                        ["text", "has"],
+                        ["text", "value"],
+                        ["integer", value.decode("utf-8")],
+                    ]})
+                else:
+                    logging.info("Ignoring message: {}".format(line))
+        if claims:
+            batch(claims)
+        time.sleep(0.1)
