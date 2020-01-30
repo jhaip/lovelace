df3000b86e3ee723b475c0e7b8990bd1e8fef3b4
Jacob Haip <haipjacob@gmail.com>
2020-01-29 22:02:34 -0500

    remove unused board import from 790

--- src/standalone_processes/790__thermalPrinter.py
+++ src/standalone_processes/790__thermalPrinter.py
@@ -1,7 +1,6 @@
 from helper2 import init, claim, retract, prehook, subscription, batch, get_my_id_str
 import helper2
 import logging
-import board
 import busio
 import serial
 import adafruit_thermal_printer
