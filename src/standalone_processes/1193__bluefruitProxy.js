9db4712eb30a4624d512c6e9e0455f2e5240f0ad
Jacob Haip <haipjacob@gmail.com>
2020-02-13 20:07:57 -0500

    connect 1193 bluefruit proxy to room

--- src/standalone_processes/1193__bluefruitProxy.js
+++ src/standalone_processes/1193__bluefruitProxy.js
@@ -1,5 +1,5 @@
 
-// const { room, myId, MY_ID_STR, run } = require('../helper2')(__filename);
+const { room, myId, MY_ID_STR, run } = require('../helper2')(__filename);
 
 // var noble = require('noble');   //noble library
 var noble = require('@abandonware/noble');
@@ -40,38 +40,79 @@ const CONVERSION_FUNC = {
     [BLUEFRUIT_LIGHT_SENSOR_SERVICE]: data => data.readFloatLE(0)
 }
 
+var active_tone_characteristic = null;
+var active_neopixel_characteristic = null;
+var neopixel_cache = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+function update_neopixels(D) {
+    if (!active_neopixel_characteristic) {
+        console.log("No neopixel characteristic yet")
+        return;
+    }
+    // D = [[R,G,B], [R,G,B], ...]
+    const msg = [
+        0x00, 0x00,
+        0x01,
+        D[0][1], D[0][0], D[0][2], // Neopixel 0 color in GRB
+        D[1][1], D[1][0], D[1][2], // Neopixel 1 color in GRB
+        D[2][1], D[2][0], D[2][2], // ...
+        D[3][1], D[3][0], D[3][2],
+        D[4][1], D[4][0], D[4][2],
+        D[5][1], D[5][0], D[5][2],
+        D[6][1], D[6][0], D[6][2],
+        D[7][1], D[7][0], D[7][2],
+        D[8][1], D[8][0], D[8][2],
+        D[9][1], D[9][0], D[9][2]
+    ]
+    active_neopixel_characteristic.write(Buffer.from(msg), true, function (error) {
+        console.log('updated neopixels');
+        console.log(error);
+    });
+}
+
+function playTone(freq_hz) {
+    if (!active_tone_characteristic) {
+        console.log("No tone characteristic yet")
+        return;
+    }
+    const toneFreqByte1 = (freq_hz & 255);
+    const toneFreqByte2 = (freq_hz & (255 << 8)) >> 8;
+    // Uint16 frequency, reverse byte order. Ex: 0xB1 0x01 = 440
+    // Uint32 milliseconds to play, reverse byte order. Ex: 0xFF 0x00 0x00 0x00 = 255
+    active_tone_characteristic.write(Buffer.from([toneFreqByte1, toneFreqByte2, 0x00, 0x00, 0x00, 0x00]), true, function (error) {
+        console.log(`starting to play ${freq_hz} tone`);
+        console.log(error);
+    });
+}
+
+function stopTone() {
+    if (!active_tone_characteristic) {
+        console.log("No tone characteristic yet")
+        return;
+    }
+    active_tone_characteristic.write(Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]), true, function (error) {
+        console.log('cleared tone');
+        console.log(error);
+    });
+}
+
 // when the radio turns on, start scanning:
 noble.on('stateChange', function scan(state) {
     if (state === 'poweredOn') {    // if the radio's on, scan for this service
         noble.startScanning([], false);
     }
 });
-
-function playTone(characteristic) {
-    // characteristic.write(Buffer.from([0xB8, 0x01, 0xFF, 0x00, 0x00, 0x00]), false, function (error) {
-    //     console.log('playing tone');
-    //     console.log(error);
-    // });
-    // Uint16 frequency, reverse byte order. Ex: 0xB1 0x01 = 440
-    // Uint32 milliseconds to play, reverse byte order. Ex: 0xFF 0x00 0x00 0x00 = 255
-    characteristic.write(Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]), true, function (error) {
-        console.log('cleared tone');
-        console.log(error);
-    });
-    setTimeout(function() {
-        // 0xB1 0x01 = Uint16 440 Hz tone
-        characteristic.write(Buffer.from([0xB8, 0x01, 0x00, 0x00, 0x00, 0x00]), true, function (error) {
-            console.log('playing tone');
-            console.log(error);
-        });
-        setTimeout(function () {
-            characteristic.write(Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]), true, function (error) {
-                console.log('cleared tone');
-                console.log(error);
-            });
-        }, 200);
-    }, 100);
-}
 
 // if you discover a peripheral with the appropriate service, connect:
 // noble.on('discover', self.connect);
@@ -96,48 +137,35 @@ noble.on('discover', function (peripheral) {
                     const serviceName = SERVICE_NAMES[serviceUuid];
                     if (serviceName === 'tone') {
                         var characteristic = characteristics[0];
-                        playTone(characteristic);
+                        active_tone_characteristic = characteristic;
+                        stopTone();
                     } else if (serviceName === 'neopixel') {
                         // var characteristic = characteristics[0];
                         characteristics.forEach(characteristic => {
                             // console.log(characteristic);
                             var characteristicUuid = `${characteristic.uuid}`;
                             if (characteristicUuid === SERVICE_CHARACTERISTICS[BLUEFRUIT_NEOPIXEL_SERVICE][0]) {
+                                // set pixel pin to 8
                                 characteristic.write(Buffer.from([0x08]), true, function (error) {
-                                    console.log('wrote neopixel 0');
+                                    console.log('wrote neopixel pixel pin service');
                                     console.log(error);
                                 });
                             }
                             if (characteristicUuid === SERVICE_CHARACTERISTICS[BLUEFRUIT_NEOPIXEL_SERVICE][1]) {
+                                // set pixel pin type to 0 = WS2812 (NeoPixel), 800kHz
                                 characteristic.write(Buffer.from([0x00]), true, function (error) {
-                                    console.log('wrote neopixel 1');
+                                    console.log('wrote neopixel pixel pin type');
                                     console.log(error);
                                 });
                             }
                             if (characteristicUuid === SERVICE_CHARACTERISTICS[BLUEFRUIT_NEOPIXEL_SERVICE][2]) {
-                                const msg = [
-                                    0x00, 0x00,
-                                    0x01,
-                                    0x00, 0x00, 0x00, // Neopixel 0 color in GRB
-                                    0x05, 0x00, 0x00, // Neopixel 1 color in GRB
-                                    0x10, 0x00, 0x00, // ...
-                                    0x00, 0x05, 0x00,
-                                    0x00, 0x10, 0x00,
-                                    0x00, 0x00, 0x05,
-                                    0x00, 0x00, 0x10,
-                                    0x00, 0x00, 0x00,
-                                    0x00, 0x00, 0x00,
-                                    0x00, 0x00, 0x00
-                                ]
-                                characteristic.write(Buffer.from(msg), true, function (error) {
-                                    console.log('wrote neopixel 2');
-                                    console.log(error);
-                                });
+                                active_neopixel_characteristic = characteristic;
+                                update_neopixels(neopixel_cache);
                             }
                             if (characteristicUuid === SERVICE_CHARACTERISTICS[BLUEFRUIT_NEOPIXEL_SERVICE][3]) {
                                 // set buffer size to 30
                                 characteristic.write(Buffer.from([0x1E, 0x00]), true, function (error) {
-                                    console.log('wrote neopixel 3');
+                                    console.log('wrote neopixel pixel buffer size');
                                     console.log(error);
                                 });
                             }
@@ -146,7 +174,24 @@ noble.on('discover', function (peripheral) {
                         var characteristic = characteristics[0];
 
                         characteristic.on('data', function (data, isNotification) {
-                            console.log(`${SERVICE_NAMES[serviceUuid]} is now: ${CONVERSION_FUNC[serviceUuid](data)}`);
+                            let characteristicValue = CONVERSION_FUNC[serviceUuid](data);
+                            console.log(`${SERVICE_NAMES[serviceUuid]} is now: ${characteristicValue}`);
+
+                            if (SERVICE_NAMES[serviceUuid] === 'buttons') {
+                                let slideValue = (characteristicValue & 1);
+                                let buttonValueA = (characteristicValue & 2) >> 1;
+                                let buttonValueB = (characteristicValue & 4) >> 2;
+                                room.cleanup();
+                                room.assert(`circuit playground "SLIDE" has value ${slideValue}`);
+                                room.assert(`circuit playground "BUTTON_A" has value ${buttonValueA}`);
+                                room.assert(`circuit playground "BUTTON_B" has value ${buttonValueB}`);
+                                room.flush();
+                            } else if (SERVICE_NAMES[serviceUuid] === 'light_sensor') {
+                                let lightValue = Math.floor(characteristicValue);
+                                room.cleanup();
+                                room.assert(`circuit playground "LIGHT" has value ${lightValue}`);
+                                room.flush();
+                            }
                         });
 
                         // to enable notify
@@ -160,8 +205,45 @@ noble.on('discover', function (peripheral) {
     });
 });
 
+room.on(`wish circuit playground neopixel $i had color $r $g $b`,
+    results => {
+        room.subscriptionPrefix(1);
+        if (!!results) {
+            results.forEach(({ i, r, g, b }) => {
+                neopixel_cache[i] = [r, g, b];
+                update_neopixels(neopixel_cache);
+            });
+        }
+        room.subscriptionPostfix();
+    }
+)
+
+room.on(`wish circuit playground played $freq tone`,
+    results => {
+        room.subscriptionPrefix(1);
+        if (!!results) {
+            results.forEach(({ freq }) => {
+                playTone(freq);
+            });
+        }
+        room.subscriptionPostfix();
+    }
+)
+
+room.on(`wish circuit playground stopped playing tone`,
+    results => {
+        room.subscriptionPrefix(1);
+        if (!!results) {
+            results.forEach(({ freq }) => {
+                stopTone();
+            });
+        }
+        room.subscriptionPostfix();
+    }
+)
+
 // Run with:
 // which node
 // sudo the-node-from-above 1193__bluefruitProxy.js
 
-// run();
+run();
