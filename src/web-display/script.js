125de5e4d2907cec5222966aa7e58d75e983c083
Jacob Haip <haipjacob@gmail.com>
2020-02-23 14:35:53 -0500

    connect to websockets and log messages

--- src/web-display/script.js
+++ src/web-display/script.js
@@ -132,6 +132,22 @@ function update(calibration, graphics) {
 // curl - X POST http://10.0.0.38:5000/cleanup-claim -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"claim":"region region1 at 176 20 1736 13 1820 980 176 1016", "retract": "$ $ wish paper $ at $ would be printed"}'
 // curl - X POST http://10.0.0.38:5000/cleanup-claim -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"claim":"region region1 has name calibration", "retract": "$ $ wish paper $ at $ would be printed"}'
 
+// wss: protocol is equivalent of https: 
+// ws:  protocol is equivalent of http:
+// You ALWAYS need to provide absolute address
+// I mean, you can't just use relative path like /echo
+const socketProtocol = (window.location.protocol === 'https:' ? 'wss:' : 'ws:')
+const echoSocketUrl = socketProtocol + '//' + window.location.hostname + '/echo/'
+const socket = new WebSocket(echoSocketUrl);
+
+socket.onopen = () => {
+    socket.send('Here\'s some text that the server is urgently awaiting!');
+}
+
+socket.onmessage = e => {
+    console.log('Message from server:', event.data)
+}
+
 async function loop() {
     try {
         const response = await fetch('/status')
