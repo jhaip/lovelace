dd58908319b60fb3453563c14e3689e40cffe35d
Jacob Haip <haipjacob@gmail.com>
2020-03-18 11:06:02 -0400

    update minimal pipeline for lua

--- src/lua/test.lua
+++ src/lua/test.lua
@@ -1,9 +1,15 @@
 require "zhelpers"
 local zmq = require "lzmq"
+local json = require "json"
+-- sudo luarocks install uuid
+local socket = require("socket")  -- gettime() has higher precision than os.time()
+local uuid = require("uuid")
+uuid.seed()
 
 local MY_ID_STR = "1999"
 local RPC_URL = "10.0.0.22" -- "localhost" -- "10.0.0.22"
-local init_ping_id = "bd096d5b-e5bb-4425-8c8a-3d109f53a264"
+local init_ping_id = uuid()
+subscription_ids = {}
 
 -- Prepare our context and publisher
 print("precontext")
@@ -31,3 +37,41 @@ print("received!")
 print(raw_msg)
 print(raw_msg[0]) -- nil
 print(raw_msg[1]) -- "1999bd096d5b-e5bb-4425-8c8a-3d109f53a2641584412138839"
+
+-- TODO: prehook
+
+-- TODO: subcribe to stuff
+function subscribe(query_strings, callback)
+    subscriptionId = uuid()
+    query = {id = subscription_id, facts = query_strings}
+    query_msg = json.encode(query)
+    subscription_ids[subscription_id] = callback
+    msg = "SUBSCRIBE" .. MY_ID_STR .. query_msg
+    print(msg)
+    local err_sub = client:send_multipart{msg}
+    print("send sub!")
+    print(err_sub)
+end
+
+function sub_callback(results)
+    print("INSIDE CALLBACK!")
+    print(results)
+end
+
+subcribe({"$ $ I am a turtle card"}, sub_callback)
+
+-- TODO: listen loop
+function listen(blocking)
+    flags = 0
+    if blocking == false
+        flags = 1 -- zmq.NOBLOCK
+    end
+    local raw_msg = client:recv_multipart(flags)
+    print("received!")
+    print(raw_msg)
+    print(raw_msg[0]) -- nil
+    print(raw_msg[1]) -- "1999bd096d5b-e5bb-4425-8c8a-3d109f53a2641584412138839"
+    print("TODO")
+end
+
+listen(false)
