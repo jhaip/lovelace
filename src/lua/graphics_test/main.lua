bacb6c7c6e55b14f85062c121fbc10c60af3d15b
Jacob Haip <haipjacob@gmail.com>
2020-03-19 23:06:33 -0400

    start building our love2d graphics output

--- src/lua/graphics_test/main.lua
+++ src/lua/graphics_test/main.lua
@@ -1,9 +1,12 @@
 require "zhelpers"
 local room = require "helper"
+local json = require "json"
 
 cwide = 520
 chigh = 333
 show_graphics = false
+graphics_cache = {}
+font = false
 
 room.on({"$ $ I am a turtle card"}, function(results)
     if #results > 0 then
@@ -13,12 +16,22 @@ room.on({"$ $ I am a turtle card"}, function(results)
     end
 end)
 
+room.on({"$ $ draw graphics $graphics on web2"}, function(results)
+    graphics_cache = {}
+    for i = 1, #results do
+        local parsedGraphics = json.decode(results[i].graphics)
+        for g = 1, #parsedGraphics do
+            graphics_cache[#graphics_cache + 1] = parsedGraphics[g]
+        end
+    end
+end)
+
 function love.load()
     love.window.setTitle(' Hello Wörld ')
     love.window.setMode(cwide, chigh)
     love.window.setFullscreen( true )
     love.graphics.setBackgroundColor(0, 0, 0)
-    font = love.graphics.setNewFont("FreeSans.ttf", 72)
+    font = love.graphics.newFont(72)
     room.init(true)
 end
 
@@ -27,6 +40,81 @@ function love.draw()
         love.graphics.setColor(255, 255, 0)
         love.graphics.print("Hello World", cwide/4, chigh/3.33)
         love.graphics.circle("fill", cwide/2, chigh/2, 50)
+    end
+    -- TODO: set baseline things
+    is_fill_on = true
+    fill_color = {255, 255, 255}
+    is_stroke_on = true
+    stroke_color = {255, 255, 255}
+    stroke_width = 1
+    love.graphics.setLineWidth( stroke_width )
+    font_color = {255, 255, 255}
+    local fontSize = 72
+    love.graphics.setFont(font)
+
+    for i = 1, #graphics_cache do
+        local g = graphics_cache[i]
+        local opt = g.options
+        if g.type == "rectangle" then
+            if is_fill_on then
+                love.graphics.setColor(fill_color)
+                love.graphics.rectangle("fill", opt.x, opt.y, opt.w, opt.h)
+            end
+            if is_stroke_on then
+                love.graphics.setColor(stroke_color)
+                love.graphics.rectangle("line", opt.x, opt.y, opt.w, opt.h)
+            end
+        elseif g.type == "ellipse" then
+            if is_fill_on then
+                love.graphics.setColor(fill_color)
+                love.graphics.ellipse("fill", opt.x, opt.y, opt.w * 0.5, opt.h * 0.5)
+            end
+            if is_stroke_on then
+                love.graphics.setColor(stroke_color)
+                love.graphics.ellipse("line", opt.x, opt.y, opt.w * 0.5, opt.h * 0.5)
+            end
+        elseif g.type == "text" then
+            love.graphics.setColor(font_color)
+            local lineHeight = fontSize * 1.3
+            for line in opt.text:gmatch("([^\n]*)\n?") do
+                love.graphics.print(line, opt.x, opt.y + i * lineHeight)
+            end
+        elseif g.type == "fill" then
+            is_fill_on = true
+            if type(opt) == "string" then
+                -- TODO: have color names like "red" in love2d
+            elseif #opt == 3 then
+                fill_color = {opt[1], opt[2], opt[3]}
+            elseif #opt == 4 then
+                fill_color = {opt[1], opt[2], opt[3], opt[4]}
+            end
+        elseif g.type == "stroke" then
+            is_stroke_on = true
+            if type(opt) == "string" then
+                -- TODO: have color names like "red" in love2d
+            elseif #opt == 3 then
+                stroke_color = {opt[1], opt[2], opt[3]}
+            elseif #opt == 4 then
+                stroke_color = {opt[1], opt[2], opt[3], opt[4]}
+            end
+        elseif g.type == "fontcolor" then
+            if type(opt) == "string" then
+                -- TODO: have color names like "red" in love2d
+            elseif #opt == 3 then
+                font_color = {opt[1], opt[2], opt[3]}
+            elseif #opt == 4 then
+                font_color = {opt[1], opt[2], opt[3], opt[4]}
+            end
+        elseif g.type == "nofill" then
+            is_fill_on = false
+        elseif g.type == "nostroke" then
+            is_stroke_on = false
+        elseif g.type == "strokewidth" then
+            stroke_width = tonumber(opt)
+            love.graphics.setLineWidth( stroke_width )
+        elseif g.type == "fontsize" then
+            is_stroke_on = false
+        end
     end
 end
 
