8b4c57f40e55632940fe8d04cd6327c6f81e48bd
Jacob Haip <haipjacob@gmail.com>
2020-04-01 00:04:27 -0400

    use logging instead of print and return if arguments are wrong

--- src/standalone_processes/100__serverWatchProcessStart.py
+++ src/standalone_processes/100__serverWatchProcessStart.py
@@ -7,39 +7,40 @@ import sys
 import os, signal
 
 my_pid = int(os.getpid())
-print(my_pid)
+logging.info(my_pid)
 
 def check_kill_process(pstring):
     for line in os.popen("ps ax | grep \"" + pstring + "\" | grep -v grep"):
         fields = line.split()
         pid = fields[0]
         if int(pid) == my_pid:
-            print("It's my own PID!")
+            logging.info("It's my own PID!")
         else:
-            print("not my PID {}".format(pid))
+            logging.info("not my PID {}".format(pid))
             os.kill(int(pid), signal.SIGKILL)
 
 helper2.rpc_url = "10.0.0.22"
 
 if len(sys.argv) != 2:
-    print("Expected a single argument of the process to run!")
+    logging.info("Expected a single argument of the process to run!")
+    return 1
 
 process_name = sys.argv[1]
 
 @prehook
 def my_prehook():
     # Kill process
-    print("killing process")
+    logging.info("killing process")
     # print("pkill -f \"{}\"".format(process_name))
     # os.system("pkill -f \"{}\"".format(process_name))
     check_kill_process(process_name)
     # Restart process
-    print("starting new process: python3 {} &".format(process_name))
+    logging.info("starting new process: python3 {} &".format(process_name))
     os.system("python3 {} &".format(process_name))
 
 init(__file__, skipListening=True)
 
 while True:
     time.sleep(10)
-    print("checking server connection")
+    logging.info("checking server connection")
     check_server_connection()
