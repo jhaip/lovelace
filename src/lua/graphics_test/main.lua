ade9df484f89cd869776840ef79d5c518fd501d7
Jacob Haip <haipjacob@gmail.com>
2020-04-30 20:08:13 -0400

    unpack args

--- src/lua/graphics_test/main.lua
+++ src/lua/graphics_test/main.lua
@@ -162,7 +162,7 @@ end)
 function love.load(args)
     local MY_ID_STR = "1999"
     print("ARGS:")
-    print(args)
+    print(unpack(args))
     if #args >= 2 then
         MY_ID_STR = args[1]
         print("Set MY_ID_STR to:")
