36b7e904c2123ccbf10113b0dfdc42d474da7d72
Jacob Haip <haipjacob@gmail.com>
2020-05-08 17:31:44 -0400

    add 286 add generic calibration adapter for laser regions

--- src/standalone_processes/111__rfidEditorBoot.js
+++ src/standalone_processes/111__rfidEditorBoot.js
@@ -99,6 +99,12 @@ room.assert(`camera 99 sees paper 980 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2)
 // Keyboard
 // room.assert(`camera 99 sees paper 649 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
 
+// region calibration adapter
+room.assert(`camera 99 sees paper 286 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
+const w = 1280;
+const h = 720;
+const o = 200;
+room.assert(`camera "1997" should calibrate to ${o} ${o} ${w-o} ${o} ${w-o} ${h-o} ${o} ${h-o} on display "1997"`)
 // Maker laser region
 // room.assert(`camera 99 sees paper 280 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
 // Laser in region
