92bca5f23e0efa3de1c3dfa3c5bdae5ddb605bd6
Jacob Haip <haipjacob@gmail.com>
2020-05-08 21:32:58 -0400

    force camera and display id to be text

--- src/standalone_processes/111__rfidEditorBoot.js
+++ src/standalone_processes/111__rfidEditorBoot.js
@@ -104,7 +104,7 @@ room.assert(`camera 99 sees paper 286 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2)
 const w = 1280;
 const h = 720;
 const o = 200;
-room.assert(`camera "1997" should calibrate to ${o} ${o} ${w-o} ${o} ${w-o} ${h-o} ${o} ${h-o} on display "1997"`)
+room.assert(`camera`, ["text", "1997"], `should calibrate to ${o} ${o} ${w - o} ${o} ${w - o} ${h - o} ${o} ${h - o} on display`, ["text", "1997"])
 // screen region calibration adapter
 room.assert(`camera 99 sees paper 287 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
 // Maker laser region
