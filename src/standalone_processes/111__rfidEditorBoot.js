f1b94b6e63d259d81af06ec645b522bdba5b8b4d
Jacob Haip <haipjacob@gmail.com>
2020-05-08 23:24:27 -0400

    add 1803 to rfid boot

--- src/standalone_processes/111__rfidEditorBoot.js
+++ src/standalone_processes/111__rfidEditorBoot.js
@@ -115,6 +115,8 @@ room.assert(`camera 99 sees paper 281 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2)
 room.assert(`camera 99 sees paper 283 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
 // Laser in Calendar
 room.assert(`camera 99 sees paper 284 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
+// Calendar Helper (highlight date)
+room.assert(`camera 99 sees paper 1803 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
 // Regiom debug viewwer
 room.assert(`camera 99 sees paper 12 at TL (0, 0) TR (2, 1) BR (2, 2) BL (1, 2) @ 1`)
 
