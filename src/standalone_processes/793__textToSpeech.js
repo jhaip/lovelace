e41ec7256eb96a944812c38cb2c8d13a2b75a83f
Jacob Haip <haipjacob@gmail.com>
2020-07-13 21:14:06 -0400

    switch to flite for text-to-speech

--- src/standalone_processes/793__textToSpeech.js
+++ src/standalone_processes/793__textToSpeech.js
@@ -7,9 +7,9 @@ room.onGetSource('wisherId',
         room.subscriptionPrefix(1);
         if (!!results && results.length > 0) {
             results.forEach(({ wisherId, text }) => {
-                runArgs = ["-ven+m7", text, "2>/dev/null"]
+                runArgs = ["-voice", "rms", "-t"]
                 console.log(runArgs)
-                const child = spawn('espeak', runArgs)
+                const child = spawn('flite', runArgs)
                 console.log("done")
                 room.retractFromSource(wisherId, `wish speaker said $`)
             });
